5027653ffbc579c6ec5be792b991396deafc952d
cjy <ajwwlswodus@naver.com>
2023-09-08 13:56:11 +0900

    보안그룹 개발 중간

--- src/stores/resources/securityGroups.js
+++ src/stores/resources/securityGroups.js
@@ -23,6 +23,7 @@ import { LIST_DEFAULT_ORDER } from 'utils/constants'
 import ObjectMapper from 'utils/object.mapper'
 import cookie from 'utils/cookie'
 
+import axios from "axios";
 
 import Base from '../basemm3' // mm3 관련 추가 파일
 import List from '../base.list'
@@ -35,6 +36,108 @@ export default class SecurityGroupStore extends Base {
 
     getResourceUrl = (params = {}) => `edgetron/resources/kubevirt/security_groups`
     getListUrl = this.getResourceUrl
+
+    @action
+    async fetchList({
+        cluster,
+        workspace,
+        namespace,
+        more,
+        devops,
+        ...params
+    } = {}) {
+        this.list.isLoading = true
+
+        if (!params.sortBy && params.ascending === undefined) {
+            params.sortBy = LIST_DEFAULT_ORDER[this.module] || 'timestamp'
+        }
+
+        if (params.limit === Infinity || params.limit === -1) {
+            params.limit = -1
+            params.page = 1
+        }
+
+        params.limit = params.limit || 10
+
+        const result = await request.get(
+            this.getResourceUrl({ cluster, workspace, namespace, devops }),
+            this.getFilterParams(params)
+        )
+
+        // mm3 api 관련 
+        const mm3Array = ['vms', 'images', 'flavors', 'networks', 'routers', 'floating_ips', 'lbs', 'security_groups', 'keypairs', 'host_devices', 'pci_devices', 'volumes', 'clusters', 'workspaces', 'licenses', 'distro_types']
+        const apiName = mm3Array.includes(this.module) ? this.module : "";
+
+        const data = (get(result, apiName) || []).map(item => ({
+            cluster,
+            namespace,
+            ...this.mapper(item),
+        }))
+
+        // security_group rull count 정보 추가
+        const dataArray = [];
+
+        const promises = data.map(async (security_group) => {
+            const securityDetail = await axios.get("/edgetron/resources/kubevirt/security_groups/" + security_group.name);
+            security_group.egress_count = (securityDetail.data.security_group.rules).filter(el => el.direction == "egress").length;
+            security_group.ingress_count = (securityDetail.data.security_group.rules).filter(el => el.direction == "ingress").length;
+            dataArray.push(security_group);
+        })
+        await Promise.all(promises);
+
+        // 초기 데이터 처리 
+        this.dataList = dataArray;
+
+        // 검색 관련 처리 
+        const exceptionArray = ['page', 'limit', 'sortBy', 'ascending'];
+        const searchArray = Object.keys(params).map((key) => {
+            let value = params[key];
+            let searchData = {
+                "searchKeywordType": key,
+                "searchKeywordText": value
+            }
+            return searchData
+        }).filter((row) => exceptionArray.includes(row.searchKeywordType) === false)
+
+        if (searchArray.length > 0) {
+            searchArray.map((search) => {
+                let resultList = this.dataList.filter((row) => {
+                    return row[search.searchKeywordType]?.toLowerCase().includes(search.searchKeywordText.toLowerCase());
+                });
+                this.searchList = resultList;
+            })
+            this.dataList = this.searchList;
+        }
+
+        //정렬 처리
+        const sortType = !!params.ascending ? "asc" : "desc";
+        this.dataList.sort((a, b) => {
+            var x = a[params.sortBy];
+            var y = b[params.sortBy];
+            if (sortType == "desc") {
+                return x > y ? -1 : x < y ? 1 : 0;
+            } else if (sortType == "asc") {
+                return x < y ? -1 : x > y ? 1 : 0;
+            }
+        });
+
+        // mm3 데이터 page 별 Slice 처리 
+        const perPage = Number(params.limit) || 10;
+        const currentPage = Number(params.page) || 1;
+        const mm3SliceData = this.dataList.slice((currentPage - 1) * perPage, (currentPage) * perPage);
+
+        this.list.update({
+            data: more ? [...this.list.data, ...mm3SliceData] : mm3SliceData,
+            total: result.totalItems || result.total_count || this.dataList.length || 0,
+            ...params,
+            limit: Number(params.limit) || 10,
+            page: Number(params.page) || 1,
+            isLoading: false,
+            ...(this.list.silent ? {} : { selectedRowKeys: [] }),
+        })
+
+        return data
+    }
 
 
     @action
