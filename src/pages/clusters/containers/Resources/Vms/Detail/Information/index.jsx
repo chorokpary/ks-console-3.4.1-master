2b806023bfcd52dd60efee34a62412ba1937901b
chorokpary <chorokpary@nate.com>
2023-10-27 15:29:30 +0900

    Custome Css 파일 분리 및 적용 작업

--- src/pages/clusters/containers/Resources/Vms/Detail/Information/index.jsx
+++ src/pages/clusters/containers/Resources/Vms/Detail/Information/index.jsx
@@ -7,8 +7,6 @@ import axios from "axios";
 import * as common from 'utils/resources'
 
 import styles from './index.scss'
-// import '../../../../Overview/CustomDashboard/custom_icon.css'
-// import '../../../../Overview/CustomDashboard/custom_style.css'
 
 const Information = (props) => {
 
--- src/pages/console/containers/Dashboard/index.jsx
+++ src/pages/console/containers/Dashboard/index.jsx
@@ -28,6 +28,8 @@ import AdminDashboard from './Admin'
 
 import styles from './index.scss'
 
+import "../../../clusters/containers/Overview/CustomDashboard/custom_theme.css";
+
 @inject('rootStore')
 class Dashboard extends React.Component {
   constructor(props) {
