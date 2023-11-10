35776730f4becfc9102bffef2c3217a96e55632a
chorokpary <chorokpary@nate.com>
2023-11-10 09:31:41 +0900

    텍스트 수정 작업

--- src/pages/clusters/containers/Resources/components/DetailVmList/index.jsx
+++ src/pages/clusters/containers/Resources/components/DetailVmList/index.jsx
@@ -224,7 +224,7 @@ const DetailVmList = (props) => {
         <div className={styles.content}>
           <div className={styles.text}>
               <div>{obj.name}</div>
-              <p>{getLocalTime(obj.creation_timestamp).format('YYYY-MM-DD HH:mm:ss')}</p>
+              <p>{getLocalTime(obj.creation_timestamp).format('YYYY-MM-DD HH:mm:ss')}에 생성 됨</p>
           </div>
           <div className={styles.text}>
               <div>{obj.state}</div>
