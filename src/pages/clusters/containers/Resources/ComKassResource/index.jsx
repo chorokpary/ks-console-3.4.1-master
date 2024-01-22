e32fd3b4b786b62bc6fff4673ea8f27bcc42ddbf
chorokpary <chorokpary@nate.com>
2024-01-22 13:32:30 +0900

    jira 81 컴퓨팅/KaaS 리소스 페이지 이슈 작업

--- src/pages/clusters/containers/Resources/ComKassResource/index.jsx
+++ src/pages/clusters/containers/Resources/ComKassResource/index.jsx
@@ -75,11 +75,6 @@ const index = (props) => {
       const vmId = get(obj, 'id')
       promsql_pod_vm_list += promsql_pod_vm_list != "" ? ("|" + vmId) : vmId;
     })
-    let promsql_pod_kaas_list = ""
-    kaasList.map((obj) => {
-      const kaasName = get(obj, 'name')
-      promsql_pod_kaas_list += promsql_pod_kaas_list != "" ? ("|" + kaasName + '.*') : kaasName + '.*';
-    })
 
     const paramsData = Object.assign(params, {
       start: params.start,
