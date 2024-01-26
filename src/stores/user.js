483244646e85fe42324481121c4838b94415a776
chorokpary <chorokpary@nate.com>
2024-01-26 11:55:29 +0900

    jira 79 일반 사용자 계정으로 컴퓨팅 자원 접근 권한 이슈 및 컴퓨팅 관련 자원 권한 설정 이슈 수정 작업

--- src/stores/user.js
+++ src/stores/user.js
@@ -93,7 +93,11 @@ export default class UsersStore extends Base {
     } else if (params.cluster) {
       module = 'clusterroles'
     }
-               
+
+    // 멤버관련 프로젝트 Role 추출 시작
+    const projectMemberRoleResp = await request.get('/kapis/iam.kubesphere.io/v1alpha2/namespaces/ked-project/members?name='+name)
+    const projectMemberRole = get(projectMemberRoleResp['items'][0],"metadata.annotations['iam.kubesphere.io/role']", {})
+                 
     const resp = await request.get(
       `kapis/iam.kubesphere.io/v1alpha2${this.getPath(params)}/${this.getModule(
         params
