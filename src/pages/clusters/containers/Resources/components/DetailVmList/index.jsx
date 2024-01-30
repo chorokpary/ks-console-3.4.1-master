4c9c7ad882f4622c758d7acbea8df31285248d98
chorokpary <chorokpary@nate.com>
2024-01-30 10:32:45 +0900

    CPU Usage 관련 promql 수정 작업

--- src/pages/clusters/containers/Resources/components/DetailVmList/index.jsx
+++ src/pages/clusters/containers/Resources/components/DetailVmList/index.jsx
@@ -150,7 +150,7 @@ const DetailVmList = (props) => {
 
     const getVmCpuUsageData = async () => {
       const vmCpuData = await customStore.fetchMetric({
-        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100)) / 100`,
+        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100))`,
         ...paramsData,
       })
 
