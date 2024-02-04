5e58aef0a72488c587213188bf7e8058dc15bff3
Jian Li <pyguni@gmail.com>
2024-02-04 09:11:49 -0500

    Revert "CPU Usage 관련 promql 수정 작업"
    This reverts commit 4c9c7ad882f4622c758d7acbea8df31285248d98.

--- src/pages/clusters/containers/Resources/Vms/Detail/Status/index.jsx
+++ src/pages/clusters/containers/Resources/Vms/Detail/Status/index.jsx
@@ -157,7 +157,7 @@ const Status = props => {
 
     const getVmCpuUsageData = async () => {
       const vmCpuData = await customStore.fetchMetric({
-        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100))`,
+        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100)) / 100`,
         ...paramsData,
       });
 
--- src/pages/projects/containers/Resources/Vms/Detail/Monitoring/index.jsx
+++ src/pages/projects/containers/Resources/Vms/Detail/Monitoring/index.jsx
@@ -69,7 +69,7 @@ const index = (props) => {
 
     const getVmCpuUsageData = async () => {
       const vmCpuData = await customStore.fetchMetric({
-        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100))`,
+        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100)) / 100`,
         // expr: `(1 - avg(irate(node_cpu_seconds_total{mode="idle"}[5m])) by (instance))`,
         ...paramsData,
       })
