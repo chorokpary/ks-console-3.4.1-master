d82374c77bed0b1ecffd30cdf7cc620aec3ff32f
chorokpary <chorokpary@nate.com>
2024-02-05 09:11:56 +0900

    상세 페이지 좌측 상단 아이콘 관련 수정 작업

--- src/pages/clusters/containers/Resources/Sriov/Detail/index.jsx
+++ src/pages/clusters/containers/Resources/Sriov/Detail/index.jsx
@@ -138,8 +138,12 @@ const KeypairDetail = props => {
     return <Loading className="ks-page-loading" />;
   }
 
+  const getBanner = () => {
+    return <i className="ico-type24-sriov"></i>
+  }
+
   const sideProps = {
-    icon: 'network-duotone',
+    icon: getBanner(),
     module: store.module,
     name: get(store.detail, 'name'),
     desc: get(store.detail.flavor, 'description', ''),
--- src/pages/clusters/containers/Resources/Vms/Detail/index.jsx
+++ src/pages/clusters/containers/Resources/Vms/Detail/index.jsx
@@ -302,8 +302,8 @@ const VmDetail = props => {
   }
 
   const getBanner = () => {
-    return <Icon name="templet" size={40} />;
-  };
+    return <i className="ico-type24-vm"></i>
+  }
 
   const sideProps = {
     icon: getBanner(),
