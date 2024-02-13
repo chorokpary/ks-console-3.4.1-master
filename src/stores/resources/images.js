37a63bbb0832b8effb2ad3205d8f40d18e3f57cc
chorokpary <chorokpary@nate.com>
2024-02-13 16:10:53 +0900

    jira 107 모든 페이지들에서 상태 관리 개선 작업 중

--- src/stores/resources/images.js
+++ src/stores/resources/images.js
@@ -37,43 +37,120 @@ export default class ImageStore extends Base {
     getListUrl = this.getResourceUrl
 
 
-    // @action
-    // async fetchList({ devops, workspace, cluster, more, ...params } = {}) {
-    //   this.list.isLoading = true
+    @action
+    async fetchList({
+      cluster,
+      workspace,
+      namespace,
+      more,
+      devops,
+      silent,
+      ...params
+    } = {}) {
+      if (!silent) {
+        this.list.isLoading = true;
+      }
+  
+      if (!params.sortBy && params.ascending === undefined) {
+        params.sortBy = LIST_DEFAULT_ORDER[this.module] || 'timestamp';
+      }
+  
+      if (params.limit === Infinity || params.limit === -1) {
+        params.limit = -1;
+        params.page = 1;
+      }
+  
+      params.limit = params.limit || 10;
+  
+      const result = await request.get(
+        this.getResourceUrl({ cluster, workspace, namespace, devops }),
+        this.getFilterParams(params)
+      );
+  
+      // mm3 api 관련
+      const mm3Array = ['vms', 'images', 'flavors', 'networks', 'routers', 'floating_ips', 'lbs', 'security_groups', 'keypairs', 'host_devices', 'pci_devices', 'volumes', 'clusters', 'workspaces', 'licenses', 'distro_types']
+      const apiName = mm3Array.includes(this.module) ? this.module : '';
+  
+      const data = (get(result, apiName) || []).map(item => ({
+        cluster,
+        namespace,
+        ...this.mapper(item),
+      }));
+  
+      // 초기 정렬 처리
+      data.sort((a, b) => {
+        return a.creation_timestamp < b.creation_timestamp
+          ? 1
+          : a.creation_timestamp > b.creation_timestamp
+          ? -1
+          : 0;
+      });
+  
+      // 초기 데이터 처리
+      this.dataList = data;
 
-    //   if (params.limit === Infinity || params.limit === -1) {
-    //     params.limit = -1
-    //     params.page = 1
-    //   }
+      // 검색 관련 처리
+      const exceptionArray = ['page', 'limit', 'sortBy', 'ascending'];
+      const searchArray = Object.keys(params)
+        .map(key => {
+          const value = params[key];
+          const searchData = {
+            searchKeywordType: key,
+            searchKeywordText: value,
+          };
+          return searchData;
+        })
+        .filter(row => exceptionArray.includes(row.searchKeywordType) === false);
+  
+      if (searchArray.length > 0) {
+        searchArray.map(search => {
+          const resultList = this.dataList.filter(row => {
 
-    //   params.limit = params.limit || 10
-
-    //   const url = `${this.getResourceUrl({ namespace: devops, cluster })}`
-
-    //   const result = await request.get(url, { ...params }, {}, () => {
-    //     return []
-    //   })
-
-    //   const data = Array.isArray(result.items)
-    //     ? result.items.map(item => {
-    //         return { ...this.mapper({ ...item, devops }) }
-    //       })
-    //     : []
-
-    //   this.list.update({
-    //     data: more ? [...this.list.data, ...data] : data,
-    //     total: result.totalItems || result.total_count || data.length || 0,
-    //     ...params,
-    //     limit: Number(params.limit) || 10,
-    //     page: Number(params.page) || 1,
-    //     isLoading: false,
-    //     ...(this.list.silent ? {} : { selectedRowKeys: [] }),
-    //   })
-
-    //   console.log(data)
-
-    //   return data
-    // }
+            return row[search.searchKeywordType]
+              ?.toLowerCase()
+              .includes(search.searchKeywordText.toLowerCase());
+          });
+          this.searchList = resultList;
+        });
+        this.dataList = this.searchList;
+      }
+  
+      // 정렬 처리
+      const sortType = params.ascending ? 'asc' : 'desc';
+      this.dataList.sort((a, b) => {
+        const x = a[params.sortBy];
+        const y = b[params.sortBy];
+        if (sortType == 'desc') {
+          return x > y ? -1 : x < y ? 1 : 0;
+        }
+        if (sortType == 'asc') {
+          return x < y ? -1 : x > y ? 1 : 0;
+        }
+      });
+  
+      // mm3 데이터 page 별 Slice 처리
+      const perPage = Number(params.limit) || 10;
+      const currentPage = Number(params.page) || 1;
+      const mm3SliceData = this.dataList.slice(
+        (currentPage - 1) * perPage,
+        currentPage * perPage
+      );
+  
+      this.list.update({
+        data: more ? [...this.list.data, ...mm3SliceData] : mm3SliceData,
+        total:
+          result.totalItems || result.total_count || this.dataList.length || 0,
+        ...params,
+        limit: Number(params.limit) || 10,
+        page: Number(params.page) || 1,
+        isLoading: false,
+        ...(this.list.silent ? {} : { selectedRowKeys: [] }),
+      });
+  
+      // console.log(this.dataList)
+  
+      return this.dataList;
+    }
 
     @action
     async create(data, params = {}) {
