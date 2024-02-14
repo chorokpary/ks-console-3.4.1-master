508a93af9cc1cd0fef65bb687395cfd44b094214
chorokpary <chorokpary@nate.com>
2024-02-14 10:28:39 +0900

    애플리케이션 배포 관리 작업 중

--- src/pages/clusters/routes/detail.js
+++ src/pages/clusters/routes/detail.js
@@ -266,7 +266,7 @@ export default [
     component: ImageBuildDetail,
   },
   {
-    path: `${PATH}/computingappdeploy/:name/:id`,
+    path: `${PATH}/computingappdeploy/:name`,
     component: ComputingAppDeployDetail,
   },
 
---
+++ src/stores/resources/appdeploy.js
@@ -0,0 +1,206 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { get, set, uniq, isArray, intersection } from 'lodash'
+import { observable, action } from 'mobx'
+import { Notify } from '@kube-design/components'
+import { LIST_DEFAULT_ORDER } from 'utils/constants'
+import ObjectMapper from 'utils/object.mapper'
+import cookie from 'utils/cookie'
+
+
+import Base from '../basemm3' // mm3 관련 추가 파일
+import List from '../base.list'
+
+export default class AppDeployStore extends Base {
+
+  records = new List()
+
+  module = 'appdeploy'
+
+  getResourceUrl = (params = {}) => `app-manager/v1alpha1/templates`
+  getListUrl = this.getResourceUrl
+  getDetailUrl = (params = {}) => `${this.getListUrl(params)}/${params.name}`
+
+  @action
+  async fetchList({
+    cluster,
+    workspace,
+    namespace,
+    more,
+    devops,
+    silent,
+    ...params
+  } = {}) {
+    // console.log("silent : "+ silent)
+    if (!silent) {
+      this.list.isLoading = true
+    }
+
+    if (!params.sortBy && params.ascending === undefined) {
+      params.sortBy = LIST_DEFAULT_ORDER[this.module] || 'timestamp'
+    }
+
+    if (params.limit === Infinity || params.limit === -1) {
+      params.limit = -1
+      params.page = 1
+    }
+
+    params.limit = params.limit || 10
+
+    const result = await request.get(
+      this.getResourceUrl()
+    )
+
+    const data = result;
+
+    // 초기 데이터 처리 
+    this.dataList = data.templates;
+
+    // 검색 관련 처리 
+    const exceptionArray = ['page', 'limit', 'sortBy', 'ascending'];
+    const searchArray = Object.keys(params).map((key) => {
+      let value = params[key];
+      let searchData = {
+        "searchKeywordType": key,
+        "searchKeywordText": value
+      }
+      return searchData
+    }).filter((row) => exceptionArray.includes(row.searchKeywordType) === false)
+
+    if (searchArray.length > 0) {
+      searchArray.map((search) => {
+        let resultList = this.dataList.filter((row) => {
+          return row[search.searchKeywordType]?.toLowerCase().includes(search.searchKeywordText.toLowerCase());
+        });
+        this.dataList = resultList;
+      })
+    }
+
+    //정렬 처리
+    const sortType = !!params.ascending ? "asc" : "desc";
+    this.dataList.sort((a, b) => {
+      var x = a[params.sortBy];
+      var y = b[params.sortBy];
+      if (sortType == "desc") {
+        return x > y ? -1 : x < y ? 1 : 0;
+      } else if (sortType == "asc") {
+        return x < y ? -1 : x > y ? 1 : 0;
+      }
+    });
+
+    // mm3 데이터 page 별 Slice 처리 
+    const perPage = Number(params.limit) || 10;
+    const currentPage = Number(params.page) || 1;
+    const mm3SliceData = this.dataList.slice((currentPage - 1) * perPage, (currentPage) * perPage);
+
+    this.list.update({
+      data: more ? [...this.list.data, ...mm3SliceData] : mm3SliceData,
+      total: result.totalItems || result.total_count || this.dataList.length || 0,
+      ...params,
+      limit: Number(params.limit) || 10,
+      page: Number(params.page) || 1,
+      isLoading: false,
+      ...(this.list.silent ? {} : { selectedRowKeys: [] }),
+    })
+
+    //console.log(this.dataList)
+
+    return this.dataList
+  }
+
+  @action
+  async create(data, params = {}) {
+    const url = this.getResourceUrl(params);
+
+    const jsonData = {};
+    const appdeployData = {};
+
+    appdeployData.name = data.name;
+    appdeployData.public_key = data.publicKey;
+    appdeployData.project = data.project;
+    appdeployData.description = data?.description;
+
+    jsonData.appdeploy = appdeployData;
+
+    const res = await request.post(url, jsonData)
+    return res
+  }
+
+  @action
+  async update({ name, ...params }, data) {
+
+    const jsonData = {};
+    const appdeployData = {};
+
+    appdeployData.id = data.id;
+    appdeployData.description = data?.description;
+
+    jsonData.appdeploy = appdeployData;
+
+    await this.submitting(
+      request.put(this.getDetailUrl({ name, ...params }), jsonData)
+    )
+  }
+
+
+  @action
+  async fetchDetail(params) {
+    this.isLoading = true
+
+    const result = await request.get(
+      `${this.getResourceUrl(params)}/${params.name}`
+    )
+    const detail = { ...params, ...this.mapper(result), kind: 'AppDeploy' }
+
+    console.log("detail : "+ JSON.stringify(detail))
+
+    this.detail = detail
+    this.isLoading = false
+    return detail
+  }
+
+  @action
+  async batchDelete({ rowKeys, ...params }) {
+    if (rowKeys.includes(globals.user.username)) {
+      Notify.error(t('DELETING_CURRENT_USER_NOT_ALLOWED'))
+    } else {
+      await this.submitting(
+        Promise.all(
+          rowKeys.map(id =>
+            request.delete(
+              `${this.getDetailUrl({ id, ...params })}`
+            )
+          )
+        )
+      )
+    }
+    this.list.selectedRowKeys = []
+  }
+
+  @action
+  delete(user) {
+    if (user.name === globals.user.username) {
+      Notify.error(t('DELETING_CURRENT_USER_NOT_ALLOWED'))
+      return
+    }
+
+    return this.submitting(request.delete(`${this.getDetailUrl(user)}`))
+  }
+
+}
