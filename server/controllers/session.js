785327af6d8d2020d20d1cc6a48ff4394fe4cce4
kedric <hgkim2@ntels.com>
2024-02-22 14:17:38 +0900

    Revert "server 정보 80번대로 변경"
    This reverts commit f85c55bd598fdba6df5c503b9c9a4e47c5f63bd1.

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -119,15 +119,15 @@ const handleLogin = async ctx => {
     return
   }
 
-  // const mm3Params = {
-  //   username: 'edgetron',
-  //   password: 'password',
-  // }
-  // const mmsData = await sendMm3Request({
-  //   method: 'POST',
-  //   url: '/edgetron/auth/generate_token',
-  //   params: mm3Params,
-  // })
+  const mm3Params = {
+    username: 'edgetron',
+    password: 'password',
+  }
+  const mmsData = await sendMm3Request({
+    method: 'POST',
+    url: '/edgetron/auth/generate_token',
+    params: mm3Params,
+  })
 
   const lastToken = ctx.cookies.get('token')
 
@@ -136,8 +136,8 @@ const handleLogin = async ctx => {
   ctx.cookies.set('refreshToken', user.refreshToken)
   ctx.cookies.set('referer', null)
 
-  // ctx.cookies.set('mm3AccessToken', mmsData.access_token)
-  // ctx.cookies.set('mm3RefreshToken', mmsData.refresh_token)
+  ctx.cookies.set('mm3AccessToken', mmsData.access_token)
+  ctx.cookies.set('mm3RefreshToken', mmsData.refresh_token)
 
   if (user.username === 'system:pre-registration') {
     const extraname = safeBase64.safeBtoa(user.extraname)
@@ -172,8 +172,8 @@ const handleLogout = async ctx => {
   ctx.cookies.set('refreshToken', null)
   ctx.cookies.set('oAuthLoginInfo', null)
 
-  // ctx.cookies.set('mm3AccessToken', null)
-  // ctx.cookies.set('mm3RefreshToken', null)
+  ctx.cookies.set('mm3AccessToken', null)
+  ctx.cookies.set('mm3RefreshToken', null)
 
   if (
     !isEmpty(oAuthLoginInfo) &&
