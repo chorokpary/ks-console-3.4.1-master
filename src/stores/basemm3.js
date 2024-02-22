9ea7ad58764d775113174ffd82f6ff081a0fa2fb
kedric <hgkim2@ntels.com>
2024-02-22 17:21:13 +0900

    Revert "base store 수정"
    This reverts commit bb15e57d413e43ee89d3bf74b65c394ff3b49dbb.

--- src/stores/basemm3.js
+++ src/stores/basemm3.js
@@ -76,7 +76,7 @@ export default class BaseStore {
     `${this.getWatchListUrl(params)}/${params.name}`
 
   getResourceUrl = (params = {}) =>
-    `kapis/edgestack.kubesphere.io/v1alpha1${this.getPath(params)}/${this.module
+    `kapis/resources.kubesphere.io/v1alpha3${this.getPath(params)}/${this.module
     }`
 
   getFilterParams = params => {
