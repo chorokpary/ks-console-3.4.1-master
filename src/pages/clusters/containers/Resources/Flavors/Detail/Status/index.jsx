91e0342a884835f7bffe7307e8399d7fe582e5e9
kedric <hgkim2@ntels.com>
2024-02-28 17:10:04 +0900

    jira 105 - flavor 상세 > kaas resource 표기

--- src/pages/clusters/containers/Resources/Flavors/Detail/Status/index.jsx
+++ src/pages/clusters/containers/Resources/Flavors/Detail/Status/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 import DetailVmList from 'pages/clusters/containers/Resources/components/DetailVmList'
+import DetailKaasListFlavor from 'pages/clusters/containers/Resources/components/DetailKaasListFlavor'
 
 const Status = (props) => {
   return (
-    <>  
-       {/* 가상 머신 상세 관련 샘플 */}
-       <DetailVmList type='Flavor' variables='flavor_object' name={props.match.params.name} />
-      
+    <>
+      {/* 가상 머신 상세 관련 샘플 */}
+      <DetailVmList type='Flavor' variables='flavor_object' name={props.match.params.name} />
+      <DetailKaasListFlavor type='Flavor' variables='flavor' name={props.match.params.name} />
     </>
   );
 };
