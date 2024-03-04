dc224bfb4d98787e04402b8675d3c3462a519711
kedric <hgkim2@ntels.com>
2024-03-04 09:33:50 +0900

    svt - vm 수정 > 보안그룹 전체해제 보완

--- src/pages/clusters/containers/Resources/components/Modals/Vms/Modify/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/Vms/Modify/index.jsx
@@ -22,7 +22,7 @@ const ModifyModal = (props) => {
     const onOk = props.onOk;
 
     form.current.validator(() => {
-      if(securityGroupCheckItems.length > 0){
+      if (securityGroupCheckItems.length > 0) {
         const { data } = form.current.props;
         data.id = props.store.detail.vm.id;
         data.scurityGroups = securityGroupCheckItems;
@@ -66,21 +66,21 @@ const ModifyModal = (props) => {
   };
 
   const handleSingleCheck = (checked, name, type) => {
-      if (checked) {
-        setVariables[type](prev => [...prev, name]);
-      } else {
-        setVariables[type](stateVariables[type].filter((el) => el !== name));
-      }
+    if (checked) {
+      setVariables[type](prev => [...prev, name]);
+    } else {
+      setVariables[type](stateVariables[type].filter((el) => el !== name));
+    }
   };
 
   const handleAllCheck = (checked, type) => {
-      if (checked) {
-        const nameArray = [];
-        dataListVariables[type].forEach((el) => nameArray.push(el.id));
-        setVariables[type](nameArray);
-      }else {
-         setVariables[type]([]);
-      }
+    if (checked) {
+      const nameArray = [];
+      dataListVariables[type].forEach((el) => nameArray.push(el.id));
+      setVariables[type](nameArray);
+    } else {
+      setVariables[type]([]);
+    }
   }
 
   const handleDelete = (name, type) => {
@@ -125,57 +125,57 @@ const ModifyModal = (props) => {
               }
               <div className={styles.table}>
                 <table>
-                    <colgroup>
-                        <col width="5%"/>
-                        <col width="30%"/>
-                        <col width="30%"/>
-                        <col width="20%"/>
-                        <col width="20%"/>
-                      </colgroup>
-                      <thead>
-                        <tr>
-                          <th>
-                          <Checkbox name='select-all-security' 
-                                onChange={(checked) => handleAllCheck(checked, "security")}
-                                checked={dataListVariables['security'].length > 0 && stateVariables['security'].length === dataListVariables['security'].length ? true : false}/>
-                          </th>
-                          <th><strong>{t('RESOURCES_SECURITY_GROUP_NAME')}</strong></th>
-                          <th><strong>{t('RESOURCES_DESCRIPTION')}</strong></th>
-                          <th><strong>{t('RESOURCES_INBOUND_RULE_COUNT')}</strong></th>
-                          <th><strong>{t('RESOURCES_OUTBOUND_RULE_COUNT')}</strong></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      {!securityGroupDataList?.length &&
-                          <tr>
-                            <td colSpan="5" className="no-data">
-                              <p>{t('RESOURCES_NO_RESOURCE_AVAILABLE_ALLOCATION')}</p>
-                            </td>
-                          </tr>
-                        }
-                        {securityGroupDataList?.map((data, key) => (
-                          <tr key={data.id}>
-                            <td>
-                              <Checkbox name={`select-${data.id}`} checked={stateVariables['security'].includes(data.id) ? true : false}
-                              onChange={(checked) => handleSingleCheck(checked, data.id, "security")} />
-                            </td>
-                            <td>{data.name}</td>
-                            <td>{data.description}</td>
-                            <td>{data.ingress_count}</td>
-                            <td>{data.egress_count}</td>
-                          </tr>
-                        ))}
-                      </tbody>
-                  </table> 
-                  <div className={styles.removeCheckWrapper}>
-                    {securityGroupCheckItems?.map((id) => {
-                        const name = securityGroupDataList?.filter((data) => data.id == id).map(item => item.name)[0]
-                        return <span key={name}><Button icon="close" onClick={() => handleDelete(id, "security")}>{name}</Button></span>
-                      }                    
-                    )}                      
-                  </div>
+                  <colgroup>
+                    <col width="5%" />
+                    <col width="30%" />
+                    <col width="30%" />
+                    <col width="20%" />
+                    <col width="20%" />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th>
+                        <Checkbox name='select-all-security'
+                          onChange={(checked) => handleAllCheck(checked, "security")}
+                          checked={dataListVariables['security'].length > 0 && stateVariables['security'].length === dataListVariables['security'].length ? true : false} />
+                      </th>
+                      <th><strong>{t('RESOURCES_SECURITY_GROUP_NAME')}</strong></th>
+                      <th><strong>{t('RESOURCES_DESCRIPTION')}</strong></th>
+                      <th><strong>{t('RESOURCES_INBOUND_RULE_COUNT')}</strong></th>
+                      <th><strong>{t('RESOURCES_OUTBOUND_RULE_COUNT')}</strong></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {!securityGroupDataList?.length &&
+                      <tr>
+                        <td colSpan="5" className="no-data">
+                          <p>{t('RESOURCES_NO_RESOURCE_AVAILABLE_ALLOCATION')}</p>
+                        </td>
+                      </tr>
+                    }
+                    {securityGroupDataList?.map((data, key) => (
+                      <tr key={data.id}>
+                        <td>
+                          <Checkbox name={`select-${data.id}`} checked={stateVariables['security'].includes(data.id) ? true : false}
+                            onChange={(checked) => handleSingleCheck(checked, data.id, "security")} />
+                        </td>
+                        <td>{data.name}</td>
+                        <td>{data.description}</td>
+                        <td>{data.ingress_count}</td>
+                        <td>{data.egress_count}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <div className={styles.removeCheckWrapper}>
+                  {securityGroupCheckItems?.map((id) => {
+                    const name = securityGroupDataList?.filter((data) => data.id == id).map(item => item.name)[0]
+                    return <span key={id}><Button icon="close" onClick={() => handleDelete(id, "security")}>{name}</Button></span>
+                  }
+                  )}
                 </div>
-                <div className={`form-item-error ${securityGroupCheckItems.length > 0 ? "hide" : ""}`}>{t('RESOURCES_SELECT_SECURITY_GROUP_TIP')}</div>
+              </div>
+              <div className={`form-item-error ${securityGroupCheckItems.length > 0 ? "hide" : ""}`}>{t('RESOURCES_SELECT_SECURITY_GROUP_TIP')}</div>
             </div>
           </Form.Item>
 
