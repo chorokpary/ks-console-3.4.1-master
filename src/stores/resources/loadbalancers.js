e49b384378ea29fbb541f4427c1a203f09101c60
kedric <hgkim2@ntels.com>
2024-03-13 11:21:49 +0900

    svt - list filter 보완

--- src/stores/resources/loadbalancers.js
+++ src/stores/resources/loadbalancers.js
@@ -105,9 +105,10 @@ export default class LoadBalancerStore extends Base {
             return searchData
         }).filter((row) => exceptionArray.includes(row.searchKeywordType) === false)
 
+        this.searchList = this.dataList;
         if (searchArray.length > 0) {
             searchArray.map((search) => {
-                let resultList = this.dataList.filter((row) => {
+                let resultList = this.searchList.filter((row) => {
                     if (search.searchKeywordType === 'project') {
                         return row[search.searchKeywordType]?.toLowerCase() === search.searchKeywordText.toLowerCase();
                     }
