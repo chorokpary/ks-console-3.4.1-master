038051fda3fee7c944314ccb0f1fb6a51a1266cc
mjko <mjko@ntels.com>
2024-04-18 09:05:13 +0900

    JIRA 240 LB 유형 및 프로토콜에서 ALL 제거 원복

--- src/pages/clusters/containers/Resources/components/Modals/LoadBalancers/Regist/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/LoadBalancers/Regist/index.jsx
@@ -28,6 +28,7 @@ const RegistModal = props => {
       protocol: 'TCP',
       port: '1',
     },
+    { value: 'ALL', label: 'ALL', protocol: 'TCP', port: '0-65535' },
     { value: 'FTP', label: 'FTP', protocol: 'TCP', port: '20' },
     { value: 'SSH', label: 'SSH', protocol: 'TCP', port: '22' },
     { value: 'TELNET', label: 'TELNET', protocol: 'TCP', port: '23' },
@@ -52,6 +53,7 @@ const RegistModal = props => {
   ];
 
   const protocolOptions = [
+    { value: 'ALL', label: 'ALL' },
     { value: 'TCP', label: 'TCP' },
     { value: 'UDP', label: 'UDP' },
     { value: 'ICMP', label: 'ICMP' },
