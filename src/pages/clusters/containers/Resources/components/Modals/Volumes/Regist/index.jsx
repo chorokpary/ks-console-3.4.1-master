5126d56970e132a8118b1a34032c028b3a8256e7
Jian Li <pyguni@gmail.com>
2024-05-09 05:25:20 -0400

    Support UploadImage DV type

--- src/pages/clusters/containers/Resources/components/Modals/Volumes/Regist/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/Volumes/Regist/index.jsx
@@ -108,6 +108,7 @@ const RegistModal = props => {
   const importSourceOptions = [
     { label: 'Empty', value: 'Empty' },
     { label: 'ImageVolume', value: 'ImageVolume' },
+    { label: 'UploadImage', value: 'UploadImage' },
     // { label: 'DataVolume', value: 'DataVolume' },
   ];
 
