d7b69a034344daf787e82960b4867217645f0c39
mjko <mjko@ntels.com>
2024-05-24 13:40:10 +0900

    JIRA 268 VM detail > 볼륨 언마운트 수정

--- src/pages/clusters/containers/Resources/components/Modals/Vms/VolumePop/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/Vms/VolumePop/index.jsx
@@ -1,14 +1,12 @@
-import { get } from 'lodash'
-import React, { useState, useRef, useEffect } from 'react'
+import { get } from 'lodash';
+import React, { useState, useRef, useEffect } from 'react';
 
-import { Form, Toggle, Notify } from '@kube-design/components'
-import { Modal } from 'components/Base'
-import styles from './index.scss'
+import { Form, Toggle, Notify } from '@kube-design/components';
+import { Modal } from 'components/Base';
+import VolumeStore from 'stores/resources/volumes';
+import styles from './index.scss';
 
-import VolumeStore from 'stores/resources/volumes'
-
-const VolumeModal = (props) => {
-
+const VolumeModal = props => {
   const form = useRef();
   const [modelView, setModalView] = useState(true);
   const [formData, setFormData] = useState({});
@@ -16,7 +14,7 @@ const VolumeModal = (props) => {
   const volumeStore = new VolumeStore();
 
   const [volumeList, setVolumeList] = useState([]);
-  const [volumeCheckItems, setVolumeCheckItems] = useState([])
+  const [volumeCheckItems, setVolumeCheckItems] = useState([]);
 
   const [reFetch, setReFetch] = useState(false);
 
@@ -25,42 +23,43 @@ const VolumeModal = (props) => {
 
   const closeModal = () => {
     setModalView(false);
-  }
+  };
 
   useEffect(() => {
     const getVolumeDataList = async () => {
-
       const volumeData = await volumeStore.fetchList({
         cluster: props.cluster,
         namespace: props.namespace,
       });
 
-      //볼륨 리스트 중 해당 가상머신과 연결이 되어 있건, 아무것도 연결이 안되어 있는 볼륨 리스트.
-      const volumeListData = volumeData.filter((obj) => {
-        return ((obj.used_by_vmi == vmName || !!!obj.used_by_vmi) && obj.name != `${vmName}-boot-dv`)
-      })
+      // 볼륨 리스트 중 해당 가상머신과 연결이 되어 있건, 아무것도 연결이 안되어 있는 볼륨 리스트.
+      const volumeListData = volumeData.filter(obj => {
+        return (
+          obj.used_by_vmi === vmId ||
+          !obj.used_by_vmi ||
+          obj.name === `${vmName}-boot-dv`
+        );
+      });
 
       const connectedVolumeArray = [];
-      await volumeListData.map((obj) => {
-        if (obj.used_by_vmi == vmName) {
-          connectedVolumeArray.push(obj.name)
+      await volumeListData.map(obj => {
+        if (obj.used_by_vmi === vmId) {
+          connectedVolumeArray.push(obj.name);
         }
-      })
+      });
 
       setVolumeCheckItems(connectedVolumeArray);
       setVolumeList(volumeListData);
     };
 
     getVolumeDataList();
-  }, [reFetch])
+  }, [reFetch]);
 
-    ;
   const handleVolumeToggle = (checked, name, id) => {
-
     if (checked) {
       setVolumeCheckItems(prev => [...prev, name]);
     } else {
-      setVolumeCheckItems(volumeCheckItems.filter((el) => el !== name));
+      setVolumeCheckItems(volumeCheckItems.filter(el => el !== name));
     }
 
     const data = {};
@@ -68,15 +67,18 @@ const VolumeModal = (props) => {
     data.vmId = vmId;
     data.volumeName = name;
     data.id = id;
-    data.actionType = checked ? "A" : "D";
+    data.actionType = checked ? 'A' : 'D';
 
-    volumeStore.actionState({ data, cluster: props.cluster, namespace: props.namespace }).then(() => {
-      Notify.success({ content: t('RESOURCES_PROCESSED') })
-      props.onOk();
-    }).catch((error) => {
-      setReFetch(!reFetch);
-    });
-  }
+    volumeStore
+      .actionState({ data, cluster: props.cluster, namespace: props.namespace })
+      .then(() => {
+        Notify.success({ content: t('RESOURCES_PROCESSED') });
+        props.onOk();
+      })
+      .catch(error => {
+        setReFetch(!reFetch);
+      });
+  };
 
   return (
     <>
@@ -89,8 +91,7 @@ const VolumeModal = (props) => {
         hideFooter
       >
         <Form data={formData} ref={form}>
-
-          <Form.Item label={t('')} >
+          <Form.Item label={t('')}>
             <div className={styles.wrapper}>
               <div className={styles.table}>
                 <table>
@@ -106,43 +107,71 @@ const VolumeModal = (props) => {
                   </colgroup>
                   <thead>
                     <tr>
-                      <th><strong>{t('RESOURCES_NAME')}</strong></th>
-                      <th><strong>{t('RESOURCES_ACCESS_MODE')}</strong></th>
-                      <th><strong>{t('RESOURCES_INPUT_SOURCE')}</strong></th>
-                      <th><strong>{t('RESOURCES_STOREGE_CLASS')}</strong></th>
-                      <th><strong>{t('RESOURCES_CAPACITY')}</strong></th>
-                      <th><strong>{t('RESOURCES_STATE')}</strong></th>
-                      <th><strong>{t('RESOURCES_VOLUME_LOCATION')}</strong></th>
-                      <th><strong>{t('RESOURCES_VOLUME_BINDING')}</strong></th>
+                      <th>
+                        <strong>{t('RESOURCES_NAME')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_ACCESS_MODE')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_INPUT_SOURCE')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_STOREGE_CLASS')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_CAPACITY')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_STATE')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_VOLUME_LOCATION')}</strong>
+                      </th>
+                      <th>
+                        <strong>{t('RESOURCES_VOLUME_BINDING')}</strong>
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
-
-                    {!volumeList?.length &&
+                    {!volumeList?.length && (
                       <tr>
-                        <td colSpan="7" className="no-data" style={{ textAlign: 'center' }}>
-                          <p>{t('RESOURCES_NO_RESOURCE_AVAILABLE_ALLOCATION')}</p>
+                        <td
+                          colSpan="7"
+                          className="no-data"
+                          style={{ textAlign: 'center' }}
+                        >
+                          <p>
+                            {t('RESOURCES_NO_RESOURCE_AVAILABLE_ALLOCATION')}
+                          </p>
                         </td>
                       </tr>
-                    }
-                    {volumeList?.map((data) => (
+                    )}
+                    {volumeList?.map(data => (
                       <tr key={data.name}>
-                        <td >{data.name}</td>
-                        <td >{(data.access_modes).map((mode) => (<p key={mode}>{mode}</p>))}</td>
-                        <td >{data.import_endpoint}</td>
-                        <td >{data.storage_class}</td>
-                        <td >{data.capacity}</td>
-                        <td >{data.phase}</td>
-                        <td >{data.selected_node}</td>
+                        <td>{data.name}</td>
                         <td>
-                          {(data.selected_node == props.store.detail.vm.node || !data.selected_node) &&
+                          {data.access_modes.map(mode => (
+                            <p key={mode}>{mode}</p>
+                          ))}
+                        </td>
+                        <td>{data.import_endpoint}</td>
+                        <td>{data.storage_class}</td>
+                        <td>{data.capacity}</td>
+                        <td>{data.phase}</td>
+                        <td>{data.selected_node}</td>
+                        <td>
+                          {(data.selected_node == props.store.detail.vm.node ||
+                            !data.selected_node) && (
                             <Toggle
-                              checked={volumeCheckItems.includes(data.name) ? true : false}
-                              onChange={(e) => handleVolumeToggle(e, data.name, data.id)}
+                              checked={!!volumeCheckItems.includes(data.name)}
+                              onChange={e =>
+                                handleVolumeToggle(e, data.name, data.id)
+                              }
                               onText={t('ON')}
                               offText={t('OFF')}
                             />
-                          }
+                          )}
                         </td>
                       </tr>
                     ))}
@@ -151,14 +180,10 @@ const VolumeModal = (props) => {
               </div>
             </div>
           </Form.Item>
-
-
         </Form>
       </Modal>
-
     </>
   );
 };
 
-export default VolumeModal
-
+export default VolumeModal;
