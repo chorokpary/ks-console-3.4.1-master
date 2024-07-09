a5933801b502847fc88880dc7bd0bd553a75bfdf
mjko <mjko@ntels.com>
2024-07-09 15:15:46 +0900

    다국어 진행중

--- src/pages/projects/containers/Resources/Vms/Detail/Status/index.jsx
+++ src/pages/projects/containers/Resources/Vms/Detail/Status/index.jsx
@@ -1,30 +1,28 @@
-import { get, groupBy, isEmpty } from 'lodash'
-import React, { useState, useEffect } from 'react'
-import { toJS } from 'mobx'
-import { observer, inject } from 'mobx-react'
-import classnames from 'classnames'
+import { get, groupBy, isEmpty } from 'lodash';
+import React, { useState, useEffect } from 'react';
+import { toJS } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import classnames from 'classnames';
 
-import axios from "axios";
-import { Panel, Text, Indicator } from 'components/Base'
-import { Icon, Button, Notify } from '@kube-design/components'
-import { TinyArea } from 'components/Charts'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { Panel, Text, Indicator } from 'components/Base';
+import { Icon, Button, Notify } from '@kube-design/components';
+import { TinyArea } from 'components/Charts';
+import { Link } from 'react-router-dom';
 
-import styles from './index.scss'
+import * as common from 'utils/resources';
+import { getAreaChartOps } from 'utils/monitoring';
 
-import * as common from 'utils/resources'
-import { getAreaChartOps } from 'utils/monitoring'
+import DetailSecurityGroupList from 'pages/projects/containers/Resources/components/DetailSecurityGroupList';
 
-import DetailSecurityGroupList from 'pages/projects/containers/Resources/components/DetailSecurityGroupList'
+import CustomStore from 'stores/monitoring/custom/monitor';
+import styles from './index.scss';
 
-import CustomStore from 'stores/monitoring/custom/monitor'
-
-const Status = (props) => {
-
+const Status = props => {
   const store = props.detailStore;
   const customStore = new CustomStore();
 
-  const { workspace, cluster, namespace } = props.match.params
+  const { workspace, cluster, namespace } = props.match.params;
 
   const [detailFlavor, setDetailFlavor] = useState(null);
   const [detailNetwork, setDetailNetwork] = useState([]);
@@ -34,22 +32,21 @@ const Status = (props) => {
   const [vmCpuData, setVmCpuData] = useState([]);
   const [vmMemoryData, setVmMemoryData] = useState([]);
 
-  const intiParams = { "times": 50, "step": "10m" }
+  const intiParams = { times: 50, step: '10m' };
   const [networkType, setNetworkType] = useState('network');
 
   const getPath = ({ cluster, namespace } = {}) => {
-    let path = ''
+    let path = '';
     if (cluster) {
-      path += `klusters/${cluster}`
+      path += `klusters/${cluster}`;
     }
     if (namespace) {
-      path += `/namespaces/${namespace}`
+      path += `/namespaces/${namespace}`;
     }
-    return path
-  }
+    return path;
+  };
 
   useEffect(() => {
-
     if (!store.detail.vm) return;
 
     const fnGetFlavor = async () => {
@@ -57,8 +54,7 @@ const Status = (props) => {
     };
 
     const fnGetNetwork = async () => {
-
-      const path = getPath({ cluster, namespace })
+      const path = getPath({ cluster, namespace });
 
       setDetailNetwork([]);
 
@@ -98,19 +94,24 @@ const Status = (props) => {
         });
         await Promise.all(promises);
       }
-
     };
 
     const fnGetSecurityGroup = async () => {
       setDetailSecurityGroup([]);
       const securityData = store.securigyGroupList;
-      const securityIdArray = store.detail.vm.security_groups.map(item => item.id)
-      const filterData = securityData.filter(item => securityIdArray.includes(item.id));
+      const securityIdArray = store.detail.vm.security_groups.map(
+        item => item.id
+      );
+      const filterData = securityData.filter(item =>
+        securityIdArray.includes(item.id)
+      );
       setDetailSecurityGroup(filterData);
     };
 
     const fnGetVolume = async () => {
-      const volumeData = store.volumeList?.filter(el => el.used_by_vmi == store.detail.vm?.id);
+      const volumeData = store.volumeList?.filter(
+        el => el.used_by_vmi == store.detail.vm?.id
+      );
       setDetailVolume(volumeData);
     };
 
@@ -122,64 +123,64 @@ const Status = (props) => {
   }, []);
 
   const getMinuteValue = (timeStr = '60s', hasUnit = true) => {
-    const unit = timeStr.slice(-1)
-    let value = parseFloat(timeStr)
+    const unit = timeStr.slice(-1);
+    let value = parseFloat(timeStr);
 
     switch (unit) {
       default:
       case 's':
-        break
+        break;
       case 'm':
-        value *= 60
-        break
+        value *= 60;
+        break;
       case 'h':
-        value *= 60 * 60
-        break
+        value *= 60 * 60;
+        break;
       case 'd':
-        value = value * 24 * 60 * 60
-        break
+        value = value * 24 * 60 * 60;
+        break;
     }
-    return hasUnit ? `${value}s` : value
-  }
+    return hasUnit ? `${value}s` : value;
+  };
 
   const getTimeRange = ({ step = '600s', times = 20 } = {}) => {
-    const interval = parseFloat(step) * times
-    const end = Math.floor(Date.now() / 1000)
-    const start = Math.floor(end - interval)
+    const interval = parseFloat(step) * times;
+    const end = Math.floor(Date.now() / 1000);
+    const start = Math.floor(end - interval);
 
-    return { start, end }
-  }
+    return { start, end };
+  };
 
-  const fetchData = async (params) => {
-
+  const fetchData = async params => {
     const paramsData = Object.assign(params, {
       start: params.start,
       end: params.end,
       step: getMinuteValue(params.step),
       times: params.times,
-    })
+    });
 
     if (!paramsData.start || !paramsData.end) {
-      const timeRange = getTimeRange(paramsData)
-      paramsData.start = timeRange.start
-      paramsData.end = timeRange.end
+      const timeRange = getTimeRange(paramsData);
+      paramsData.start = timeRange.start;
+      paramsData.end = timeRange.end;
     }
 
     const getVmCpuUsageData = async () => {
       const vmCpuData = await customStore.fetchMetric({
         expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[5m])) * 100)) / 100`,
         ...paramsData,
-        cluster, namespace
-      })
+        cluster,
+        namespace,
+      });
 
-      const vmCpuMetricData = _.find(vmCpuData, (data) => {
+      const vmCpuMetricData = _.find(vmCpuData, data => {
         if (data.metric.pod === store.detail.id) return data;
       });
 
-      // 배열 처리 
+      // 배열 처리
       const vmCpuArray = [];
-      !!vmCpuMetricData && vmCpuArray.push(vmCpuMetricData)
-      setVmCpuData(vmCpuArray)
+      !!vmCpuMetricData && vmCpuArray.push(vmCpuMetricData);
+      setVmCpuData(vmCpuArray);
     };
 
     // vm memory data
@@ -187,24 +188,23 @@ const Status = (props) => {
       const vmMemoryData = await customStore.fetchMetric({
         expr: `node_memory_MemTotal_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*"}-node_memory_MemFree_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*"}-node_memory_Cached_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*"}`,
         ...paramsData,
-        cluster, namespace
-      })
+        cluster,
+        namespace,
+      });
 
-      const vmMemoryMetricData = _.find(vmMemoryData, (data) => {
+      const vmMemoryMetricData = _.find(vmMemoryData, data => {
         if (data.metric.pod === store.detail.id) return data;
       });
 
-      // 배열 처리 
+      // 배열 처리
       const vmMemoryArray = [];
-      !!vmMemoryMetricData && vmMemoryArray.push(vmMemoryMetricData)
-      setVmMemoryData(vmMemoryArray)
-
+      !!vmMemoryMetricData && vmMemoryArray.push(vmMemoryMetricData);
+      setVmMemoryData(vmMemoryArray);
     };
 
     getVmCpuUsageData();
     getVmMemoryUsageData();
-
-  }
+  };
 
   const getMonitoringCfgs = () => [
     {
@@ -223,25 +223,24 @@ const Status = (props) => {
       data: vmMemoryData,
       bgColor: 'transparent',
     },
-  ]
+  ];
 
   const renderMonitorings = () => {
-
     const isExpand = false;
     const loading = false;
 
-    if (loading) return <div className={styles.monitors}>{t('LOADING')}</div>
+    if (loading) return <div className={styles.monitors}>{t('LOADING')}</div>;
 
     if (isEmpty(vmCpuData) && isEmpty(vmMemoryData))
-      return <div className={styles.monitors}>{t('NO_MONITORING_DATA')}</div>
+      return <div className={styles.monitors}>{t('NO_MONITORING_DATA')}</div>;
 
-    const configs = getMonitoringCfgs()
+    const configs = getMonitoringCfgs();
 
     return (
       <div className={styles.monitors}>
         <div className={styles.charts}>
           {configs.map(item => {
-            const config = getAreaChartOps(item)
+            const config = getAreaChartOps(item);
 
             return (
               <div key={item.type}>
@@ -253,39 +252,42 @@ const Status = (props) => {
                   darkMode={isExpand}
                 />
               </div>
-            )
+            );
           })}
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-  const getState = (state) => {
-    if (state === 'Provisioning'
-      || state === 'Starting'
-      || state === 'Stopping'
-      || state === 'Terminating'
-      || state === 'Migrating') {
-      return "waiting"
-    } else if (state === 'Running') {
-      return "running"
-    } else if (state === 'Stopped' || state === 'Paused') {
-      return "stopped"
-    } else if (state === 'Unknown') {
-      return "error"
-    } else {
-      return "error"
+  const getState = state => {
+    if (
+      state === 'Provisioning' ||
+      state === 'Starting' ||
+      state === 'Stopping' ||
+      state === 'Terminating' ||
+      state === 'Migrating'
+    ) {
+      return 'waiting';
     }
-  }
+    if (state === 'Running') {
+      return 'running';
+    }
+    if (state === 'Stopped' || state === 'Paused') {
+      return 'stopped';
+    }
+    if (state === 'Unknown') {
+      return 'error';
+    }
+    return 'error';
+  };
 
   return (
     <>
       <div>
-
         {/* 가상 머신 */}
         <Panel title={t('RESOURCES_VM')}>
           <div className={styles.wrapper}>
-            <div className={styles.itemVm} >
+            <div className={styles.itemVm}>
               <div className={styles.icon}>
                 <i className="ico-type40-vm"></i>
                 <Indicator
@@ -314,70 +316,91 @@ const Status = (props) => {
         </Panel>
 
         {/* Flavor */}
-        {!!detailFlavor &&
-          <Panel title={"Flavor"}>
+        {!!detailFlavor && (
+          <Panel title={t('RESOURCES_FLAVOR')}>
             <div className={styles.wrapper}>
               <div className={classnames(styles.itemFlavor)}>
                 <div className={styles.icon}>
                   <Icon name="apps" size={40} />
                 </div>
                 <div className={classnames(styles.title, styles.name)}>
-                  <div><Link to={`/${workspace}/clusters/${cluster}/projects/${namespace}/flavors/${detailFlavor.name}`}>{detailFlavor.name}</Link></div>
+                  <div>
+                    <Link
+                      to={`/${workspace}/clusters/${cluster}/projects/${namespace}/flavors/${detailFlavor.name}`}
+                    >
+                      {detailFlavor.name}
+                    </Link>
+                  </div>
                   <p>{t('RESOURCES_NAME')}</p>
                 </div>
                 <div className={styles.title}>
                   <div>
-                    {
-                      detailFlavor.devices.length >= 1 ?
-                        detailFlavor.devices.length == 1 ? detailFlavor.devices[0].name : detailFlavor.devices[0].name + " " + t('RESOURCES_BESIDES') + " " + (detailFlavor.devices.length - 1) + t('RESOURCES_COUNT')
-                        : "-"
-                    }
+                    {detailFlavor.devices.length >= 1
+                      ? detailFlavor.devices.length == 1
+                        ? detailFlavor.devices[0].name
+                        : `${detailFlavor.devices[0].name} ${t(
+                            'RESOURCES_BESIDES'
+                          )} ${detailFlavor.devices.length - 1}${t(
+                            'RESOURCES_COUNT'
+                          )}`
+                      : '-'}
                   </div>
                   <p>{t('RESOURCES_HOST_DEVICE')}</p>
                 </div>
                 <div className={styles.title}>
                   <Text
-                    key='CPU'
-                    icon='cpu'
-                    title={detailFlavor.vcpus + " Core"}
+                    key="CPU"
+                    icon="cpu"
+                    title={`${detailFlavor.vcpus} Core`}
                     description={t('CPU')}
                   />
                 </div>
                 <div className={styles.title}>
                   <Text
-                    key='Memory'
-                    icon='memory'
-                    title={common.fnSetBytes(detailFlavor.ram) + " Gib"}
+                    key="Memory"
+                    icon="memory"
+                    title={`${common.fnSetBytes(detailFlavor.ram)} Gib`}
                     description={t('Memory')}
                   />
                 </div>
                 <div className={styles.title}>
                   <Text
-                    key='Disk'
-                    icon='storage'
-                    title={detailFlavor.root_disk + " Gib"}
+                    key="Disk"
+                    icon="storage"
+                    title={`${detailFlavor.root_disk} Gib`}
                     description={t('Disk')}
                   />
                 </div>
                 <div className={styles.title}>
                   <Text
-                    key='GPU'
-                    icon='gpu'
-                    title={detailFlavor.gpus.length >= 1 ?
-                      detailFlavor.gpus.length == 1 ? detailFlavor.gpus[0].name : detailFlavor.gpus[0].name + " " + t('RESOURCES_BESIDES') + " " + (detailFlavor.gpus.length - 1) + t('RESOURCES_COUNT')
-                      : "-"}
+                    key="GPU"
+                    icon="gpu"
+                    title={
+                      detailFlavor.gpus.length >= 1
+                        ? detailFlavor.gpus.length == 1
+                          ? detailFlavor.gpus[0].name
+                          : `${detailFlavor.gpus[0].name} ${t(
+                              'RESOURCES_BESIDES'
+                            )} ${detailFlavor.gpus.length - 1}${t(
+                              'RESOURCES_COUNT'
+                            )}`
+                        : '-'
+                    }
                     description={t('GPU')}
                   />
                 </div>
               </div>
             </div>
           </Panel>
-        }
+        )}
 
         {/* 보안그룹 */}
-        {
-          store.detail.vm?.security_groups.length > 0 && <DetailSecurityGroupList securityGroupData={detailSecurityGroup} params={props.match.params} />
-        }
+        {store.detail.vm?.security_groups.length > 0 && (
+          <DetailSecurityGroupList
+            securityGroupData={detailSecurityGroup}
+            params={props.match.params}
+          />
+        )}
 
         {/* 네트워크 */}
         {detailNetwork.length > 0 && (
@@ -386,7 +409,7 @@ const Status = (props) => {
               {detailNetwork.map((obj, index) => (
                 <div className={classnames(styles.itemNetwork)} key={index}>
                   <div className={styles.icon}>
-                    {!!!obj.resource_name ? (
+                    {!obj.resource_name ? (
                       <Icon name={`network-duotone`} size={40} />
                     ) : (
                       <i className="ico-type-sriov"></i>
@@ -394,12 +417,16 @@ const Status = (props) => {
                   </div>
                   <div className={classnames(styles.title, styles.name)}>
                     <div>
-                      {!!!obj.resource_name ? (
-                        <Link to={`/${workspace}/clusters/${cluster}/projects/${namespace}/networks/${obj.name}/${obj.id}`}>
+                      {!obj.resource_name ? (
+                        <Link
+                          to={`/${workspace}/clusters/${cluster}/projects/${namespace}/networks/${obj.name}/${obj.id}`}
+                        >
                           {obj.name}
                         </Link>
                       ) : (
-                        <Link to={`/${workspace}/clusters/${cluster}/projects/${namespace}/sriovs/${obj.name}`}>
+                        <Link
+                          to={`/${workspace}/clusters/${cluster}/projects/${namespace}/sriovs/${obj.name}`}
+                        >
                           {obj.name}
                         </Link>
                       )}
@@ -425,7 +452,7 @@ const Status = (props) => {
         )}
 
         {/* 볼륨 */}
-        {detailVolume.length > 0 &&
+        {detailVolume.length > 0 && (
           <Panel title={t('RESOURCES_VOLUME')}>
             <div className={styles.wrapper}>
               {detailVolume.map((obj, index) => (
@@ -434,12 +461,20 @@ const Status = (props) => {
                     <Icon name="storage" size={40} />
                   </div>
                   <div className={classnames(styles.title, styles.name)}>
-                    <div><Link to={`/${workspace}/clusters/${cluster}/projects/${namespace}/resourcesvolumes/${obj.name}/${obj.id}`}>{obj.name}</Link></div>
+                    <div>
+                      <Link
+                        to={`/${workspace}/clusters/${cluster}/projects/${namespace}/resourcesvolumes/${obj.name}/${obj.id}`}
+                      >
+                        {obj.name}
+                      </Link>
+                    </div>
                     <p>{t('RESOURCES_NAME')}</p>
                   </div>
                   <div className={styles.title}>
                     <div>
-                      {(obj.access_modes).map((mode) => (<p key={mode}>{mode}</p>))}
+                      {obj.access_modes.map(mode => (
+                        <p key={mode}>{mode}</p>
+                      ))}
                     </div>
                     <p>{t('RESOURCES_ACCESS_MODE')}</p>
                   </div>
@@ -455,12 +490,10 @@ const Status = (props) => {
               ))}
             </div>
           </Panel>
-        }
-
+        )}
       </div>
     </>
   );
 };
 
-export default inject('detailStore')(observer(Status))
-
+export default inject('detailStore')(observer(Status));
