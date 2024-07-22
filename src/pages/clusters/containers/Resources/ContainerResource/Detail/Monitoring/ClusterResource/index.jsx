1ba86c6950834b2a9a43c6bac2f735ceef83cb9c
Jian Li <pyguni@gmail.com>
2024-07-22 14:08:35 +0900

    Fix: resolve the issue of VM cpu usage exceeding the 100%

--- src/pages/clusters/containers/Resources/ContainerResource/Detail/Monitoring/ClusterResource/index.jsx
+++ src/pages/clusters/containers/Resources/ContainerResource/Detail/Monitoring/ClusterResource/index.jsx
@@ -76,7 +76,7 @@ const ClusterResourceStatus = (props) => {
         const currentTime = Math.floor(Date.now() / 1000);
         return new Promise(async (resolve, reject) => {
             const cpuFetchData = await customStore.fetchMetric({
-		expr: `(sum by (pod,nodename) (irate(node_cpu_seconds_total{service="launcher-node-exporter",mode!~"guest.*|idle|iowait"}[5m])) + on(pod) group_left(nodename) node_uname_info) - 1`,
+		expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{service="launcher-node-exporter",mode="idle"}[${step}])) * ${times})) / 100`,
                 start: currentTime - 30000,
                 end: currentTime,
             })
