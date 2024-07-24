e25ec13d1f5161d78363993ce37516e1e902362a
mjko <mjko@ntels.com>
2024-07-24 09:55:57 +0900

    충돌 부분 확인

--- src/pages/clusters/containers/Resources/Vms/Detail/Monitoring/index.jsx
+++ src/pages/clusters/containers/Resources/Vms/Detail/Monitoring/index.jsx
@@ -1,19 +1,18 @@
-import { get, isEmpty, find } from 'lodash'
-import React, { useState, useEffect } from 'react'
-import { toJS } from 'mobx'
-import { observer, inject } from 'mobx-react'
-import classnames from 'classnames'
+import { get, isEmpty, find } from 'lodash';
+import React, { useState, useEffect } from 'react';
+import { toJS } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import classnames from 'classnames';
 
-import { getChartData, getAreaChartOps } from 'utils/monitoring'
-import CustomStore from 'stores/monitoring/custom/monitor'
+import { getChartData, getAreaChartOps } from 'utils/monitoring';
+import CustomStore from 'stores/monitoring/custom/monitor';
 
-import { Controller as MonitoringController } from 'components/Cards/Monitoring'
-import { SimpleArea } from 'components/Charts'
+import { Controller as MonitoringController } from 'components/Cards/Monitoring';
+import { SimpleArea } from 'components/Charts';
 
-import styles from './index.scss'
+import styles from './index.scss';
 
-const index = (props) => {
-
+const index = props => {
   const store = props.detailStore;
   const customStore = new CustomStore();
 
@@ -24,147 +23,157 @@ const index = (props) => {
   const [vmDiskData, setVmDiskData] = useState([]);
 
   const getMinuteValue = (timeStr = '60s', hasUnit = true) => {
-    const unit = timeStr.slice(-1)
-    let value = parseFloat(timeStr)
+    const unit = timeStr.slice(-1);
+    let value = parseFloat(timeStr);
 
     switch (unit) {
       default:
       case 's':
-        break
+        break;
       case 'm':
-        value *= 60
-        break
+        value *= 60;
+        break;
       case 'h':
-        value *= 60 * 60
-        break
+        value *= 60 * 60;
+        break;
       case 'd':
-        value = value * 24 * 60 * 60
-        break
+        value = value * 24 * 60 * 60;
+        break;
     }
-    return hasUnit ? `${value}s` : value
-  }
+    return hasUnit ? `${value}s` : value;
+  };
 
   const getTimeRange = ({ step = '600s', times = 20 } = {}) => {
-    const interval = parseFloat(step) * times
-    const end = Math.floor(Date.now() / 1000)
-    const start = Math.floor(end - interval)
+    const interval = parseFloat(step) * times;
+    const end = Math.floor(Date.now() / 1000);
+    const start = Math.floor(end - interval);
 
-    return { start, end }
-  }
+    return { start, end };
+  };
 
-  const fetchData = async (params) => {
-
+  const fetchData = async params => {
     const paramsData = Object.assign(params, {
       start: params.start,
       end: params.end,
       step: getMinuteValue(params.step),
       times: params.times,
-    })
+    });
 
     if (!paramsData.start || !paramsData.end) {
-      const timeRange = getTimeRange(paramsData)
-      paramsData.start = timeRange.start
-      paramsData.end = timeRange.end
+      const timeRange = getTimeRange(paramsData);
+      paramsData.start = timeRange.start;
+      paramsData.end = timeRange.end;
     }
 
     const getVmCpuUsageData = async () => {
-      const cpuLinuxDataExpr = `(sum by (pod,instance) (irate(node_cpu_seconds_total{service="launcher-node-exporter",mode!~"guest.*|idle|iowait"}[5m])) + on(pod) group_left(instance) node_uname_info) - 1`
-      const cpuWindowsDataExpr = `(sum by (pod,instance) (irate(windows_cpu_time_total{service="launcher-node-exporter",mode!~"guest.*|idle|iowait"}[5m])) + on(pod) group_left(instance) windows_os_info) - 1`
-
+      const cpuLinuxDataExpr = `((sum by (pod,instance) (irate(node_cpu_seconds_total{service="launcher-node-exporter",mode!~"guest.*|idle|iowait"}[5m])) + on(pod) group_left(instance) node_uname_info) - 1) / ${store.detail.vm.flavor.vcpus}`;
+      const cpuWindowsDataExpr = `(sum by (pod,instance) (irate(windows_cpu_time_total{service="launcher-node-exporter",mode!~"guest.*|idle|iowait"}[5m])) + on(pod) group_left(instance) windows_os_info) - 1`;
       const vmCpuData = await customStore.fetchMetric({
-	      expr: store.detail.vm.os_type == "linux" ? cpuLinuxDataExpr : cpuWindowsDataExpr,
+        expr:
+          store.detail.vm.os_type == 'linux'
+            ? cpuLinuxDataExpr
+            : cpuWindowsDataExpr,
         ...paramsData,
-        cluster: props.match.params.cluster
-      })
+        cluster: props.match.params.cluster,
+      });
 
-      const vmCpuMetricData = _.find(vmCpuData, (data) => {
+      const vmCpuMetricData = _.find(vmCpuData, data => {
         if (data.metric.pod === store.detail.id) return data;
       });
 
-      // 배열 처리 
+      // 배열 처리
       const vmCpuArray = [];
-      vmCpuArray.push(vmCpuMetricData)
-      setVmCpuData(vmCpuArray)
+      vmCpuArray.push(vmCpuMetricData);
+      setVmCpuData(vmCpuArray);
     };
 
     // vm memory data
     const getVmMemoryUsageData = async () => {
-      const memoryLinuxDataExpr = `node_memory_MemTotal_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}-node_memory_MemFree_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}-node_memory_Cached_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}`
-      const memoryWindowsDataExpr = `windows_os_visible_memory_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}-windows_memory_available_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}`
+      const memoryLinuxDataExpr = `node_memory_MemTotal_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}-node_memory_MemFree_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}-node_memory_Cached_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}`;
+      const memoryWindowsDataExpr = `windows_os_visible_memory_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}-windows_memory_available_bytes{service="launcher-node-exporter",pod!~"virt-launcher-.*"}`;
 
       const vmMemoryData = await customStore.fetchMetric({
-	      expr: store.detail.vm.os_type == "linux" ? memoryLinuxDataExpr : memoryWindowsDataExpr,
+        expr:
+          store.detail.vm.os_type == 'linux'
+            ? memoryLinuxDataExpr
+            : memoryWindowsDataExpr,
         ...paramsData,
-        cluster: props.match.params.cluster
-      })
+        cluster: props.match.params.cluster,
+      });
 
-      const vmMemoryMetricData = _.find(vmMemoryData, (data) => {
+      const vmMemoryMetricData = _.find(vmMemoryData, data => {
         if (data.metric.pod === store.detail.id) return data;
       });
 
-      // 배열 처리 
+      // 배열 처리
       const vmMemoryArray = [];
-      vmMemoryArray.push(vmMemoryMetricData)
-      setVmMemoryData(vmMemoryArray)
-
+      vmMemoryArray.push(vmMemoryMetricData);
+      setVmMemoryData(vmMemoryArray);
     };
 
     // vm inbound data
     const getVmInboundData = async () => {
-      const inboundLinuxDataExpr = `sum by (pod) (irate(node_network_receive_bytes_total{service="launcher-node-exporter",device=~"net.*"}[5m]))`
-      const inboundWindowsDataExpr = `sum by (pod) (irate(windows_net_bytes_received_total{service="launcher-node-exporter"}[5m]))`
+      const inboundLinuxDataExpr = `sum by (pod) (irate(node_network_receive_bytes_total{service="launcher-node-exporter",device=~"net.*"}[5m]))`;
+      const inboundWindowsDataExpr = `sum by (pod) (irate(windows_net_bytes_received_total{service="launcher-node-exporter"}[5m]))`;
 
       const vmInboundData = await customStore.fetchMetric({
-	expr: store.detail.vm.os_type == "linux" ? inboundLinuxDataExpr : inboundWindowsDataExpr,
+        expr:
+          store.detail.vm.os_type == 'linux'
+            ? inboundLinuxDataExpr
+            : inboundWindowsDataExpr,
         ...paramsData,
-        cluster: props.match.params.cluster
-      })
+        cluster: props.match.params.cluster,
+      });
 
-      const vmInboundMetricData = _.find(vmInboundData, (data) => {
+      const vmInboundMetricData = _.find(vmInboundData, data => {
         if (data.metric.pod === store.detail.id) return data;
       });
 
-      setVmInboundData(vmInboundMetricData)
-
+      setVmInboundData(vmInboundMetricData);
     };
 
     // vm outbound data
     const getVmOutboundData = async () => {
-      const outboundLinuxDataExpr = `sum by (pod) (irate(node_network_transmit_bytes_total{service="launcher-node-exporter",device=~"net.*"}[5m]))`
-      const outboundWindowsDataExpr = `sum by (pod) (irate(windows_net_bytes_sent_total{service="launcher-node-exporter"}[5m]))`
+      const outboundLinuxDataExpr = `sum by (pod) (irate(node_network_transmit_bytes_total{service="launcher-node-exporter",device=~"net.*"}[5m]))`;
+      const outboundWindowsDataExpr = `sum by (pod) (irate(windows_net_bytes_sent_total{service="launcher-node-exporter"}[5m]))`;
 
       const vmOutboundData = await customStore.fetchMetric({
-	expr: store.detail.vm.os_type == "linux" ? outboundLinuxDataExpr : outboundWindowsDataExpr,
+        expr:
+          store.detail.vm.os_type == 'linux'
+            ? outboundLinuxDataExpr
+            : outboundWindowsDataExpr,
         ...paramsData,
-        cluster: props.match.params.cluster
-      })
+        cluster: props.match.params.cluster,
+      });
 
-      const vmOutboundMetricData = _.find(vmOutboundData, (data) => {
+      const vmOutboundMetricData = _.find(vmOutboundData, data => {
         if (data.metric.pod === store.detail.id) return data;
       });
 
-      setVmOutboundData(vmOutboundMetricData)
-
+      setVmOutboundData(vmOutboundMetricData);
     };
 
     const getVmDiskUsageData = async () => {
-      const diskLinuxDataExpr = `(100 - (((sum by(pod) (node_filesystem_avail_bytes)) / sum by(pod) (node_filesystem_size_bytes)) * 100)) / 100`
-      const diskWindowsDataExpr = `(100 - (((sum by(pod) (windows_logical_disk_free_bytes)) / sum by(pod) (windows_logical_disk_size_bytes)) * 100)) / 100`
+      const diskLinuxDataExpr = `(100 - (((sum by(pod) (node_filesystem_avail_bytes)) / sum by(pod) (node_filesystem_size_bytes)) * 100)) / 100`;
+      const diskWindowsDataExpr = `(100 - (((sum by(pod) (windows_logical_disk_free_bytes)) / sum by(pod) (windows_logical_disk_size_bytes)) * 100)) / 100`;
 
       const vmDiskData = await customStore.fetchMetric({
-	expr: store.detail.vm.os_type == "linux" ? diskLinuxDataExpr : diskWindowsDataExpr,
+        expr:
+          store.detail.vm.os_type == 'linux'
+            ? diskLinuxDataExpr
+            : diskWindowsDataExpr,
         ...paramsData,
-        cluster: props.match.params.cluster
-      })
+        cluster: props.match.params.cluster,
+      });
 
-      const vmDiskMetricData = _.find(vmDiskData, (data) => {
+      const vmDiskMetricData = _.find(vmDiskData, data => {
         if (data.metric?.pod === store.detail.id) return data;
       });
 
-      // 배열 처리 
+      // 배열 처리
       const vmDiskArray = [];
-      vmDiskArray.push(vmDiskMetricData)
-      setVmDiskData(vmDiskArray)
+      vmDiskArray.push(vmDiskMetricData);
+      setVmDiskData(vmDiskArray);
     };
 
     getVmCpuUsageData();
@@ -172,8 +181,7 @@ const index = (props) => {
     getVmInboundData();
     getVmOutboundData();
     getVmDiskUsageData();
-
-  }
+  };
 
   const getMonitoringCfgs = () => {
     return [
@@ -206,11 +214,11 @@ const index = (props) => {
         legend: [t('RESOURCES_DISK_USAGE')],
         data: vmDiskData,
       },
-    ]
-  }
+    ];
+  };
 
-  const { isLoading, isRefreshing } = customStore
-  const configs = getMonitoringCfgs()
+  const { isLoading, isRefreshing } = customStore;
+  const configs = getMonitoringCfgs();
 
   return (
     <MonitoringController
@@ -220,14 +228,12 @@ const index = (props) => {
       refreshing={isRefreshing}
     >
       {configs.map(item => {
-        const config = getAreaChartOps(item)
-        if (isEmpty(config.data)) return null
-        return <SimpleArea key={config.title} width="100%" {...config} />
+        const config = getAreaChartOps(item);
+        if (isEmpty(config.data)) return null;
+        return <SimpleArea key={config.title} width="100%" {...config} />;
       })}
-
     </MonitoringController>
   );
 };
 
-export default inject('detailStore')(observer(index))
-
+export default inject('detailStore')(observer(index));
