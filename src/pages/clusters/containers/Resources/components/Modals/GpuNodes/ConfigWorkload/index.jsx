6942f75cefa291545384b169148aea1fd6a7a3e1
Jian Li <pyguni@gmail.com>
2024-08-26 11:39:13 +0900

    Fix: disable GPU workload config if driver_type is marked as 'none'

--- src/pages/clusters/containers/Resources/components/Modals/GpuNodes/ConfigWorkload/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/GpuNodes/ConfigWorkload/index.jsx
@@ -44,9 +44,9 @@ const ConfigGpuWorkloadType = ({ title, onOk, store, ...props }) => {
   }
 
   const workloadOptions = [
-    { label: t('RESOURCES_GPU_WORKLOAD_CONTAINER'), value: 'container', description: t('RESOURCES_GPU_WORKLOAD_CONTAINER_DESC'), disabled: store.detail.gpunode.driver_type === 'vgpu' },
+    { label: t('RESOURCES_GPU_WORKLOAD_CONTAINER'), value: 'container', description: t('RESOURCES_GPU_WORKLOAD_CONTAINER_DESC'), disabled: store.detail.gpunode.driver_type === 'vgpu' || store.detail.gpunode.driver_type === 'none' },
     { label: t('RESOURCES_GPU_WORKLOAD_VM_PASSTHROUGH'), value: 'vm-passthrough', description: t('RESOURCES_GPU_WORKLOAD_VM_PASSTHROUGH_DESC')},
-    { label: t('RESOURCES_GPU_WORKLOAD_VM_VGPU'), value: 'vm-vgpu', description: t('RESOURCES_GPU_WORKLOAD_VM_VGPU_DESC'), disabled: store.detail.gpunode.driver_type === 'native' }
+    { label: t('RESOURCES_GPU_WORKLOAD_VM_VGPU'), value: 'vm-vgpu', description: t('RESOURCES_GPU_WORKLOAD_VM_VGPU_DESC'), disabled: store.detail.gpunode.driver_type === 'native' || store.detail.gpunode.driver_type === 'none' }
   ];
 
   return (
