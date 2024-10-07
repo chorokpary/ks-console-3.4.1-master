ad6ee1292aceb93c1d512e6c688fd07c67301474
Jian Li <pyguni@gmail.com>
2024-10-07 20:39:27 +0900

    Fix: config the default lang for document as Korean

--- src/utils/index.js
+++ src/utils/index.js
@@ -446,13 +446,13 @@ export const getWebSocketProtocol = protocol => {
 }
 
 export const getWebsiteUrl = () => {
-  const useLang = get(globals, 'user.lang', 'en')
+  const useLang = get(globals, 'user.lang', 'ko')
   const lang = useLang === 'ko' ? 'ko' : 'en'
   return globals.config.documents[lang]
 }
 
 export const getCustomizedWebsiteUrl = () => {
-  const useLang = get(globals, 'user.lang', 'en')
+  const useLang = get(globals, 'user.lang', 'ko')
   const lang = useLang === 'ko' ? 'ko' : 'en'
 
   const host = window.location.hostname
