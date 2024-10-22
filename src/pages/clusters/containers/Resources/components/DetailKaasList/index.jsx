21d7349e6c84af22c38815e24233e71a8d51a4bf
changjjjjjjj <light9632@gmail.com>
2024-10-22 13:49:08 +0900

    Fix: edit KaaS volume detail - KaaS 머신과 비슷하게 ui 통일

--- src/pages/clusters/containers/Resources/components/DetailKaasList/index.jsx
+++ src/pages/clusters/containers/Resources/components/DetailKaasList/index.jsx
@@ -150,7 +150,11 @@ const DetailKaasList = props => {
             <p>{t('RESOURCES_DEPLOY_STEP')}</p>
           </div>
           <div className={styles.text}>
-            <div>{obj.cluster_ready ? t('RESOURCES_CLUSTER_READY') : t('RESOURCES_CLUSTER_NOT_READY')}</div>
+            <div>
+              {obj.cluster_ready
+                ? t('RESOURCES_CLUSTER_READY')
+                : t('RESOURCES_CLUSTER_NOT_READY')}
+            </div>
             <p>{t('RESOURCES_STATE')}</p>
           </div>
           <div className={styles.text}>
@@ -198,7 +202,9 @@ const DetailKaasList = props => {
                     <p>{t('RESOURCES_NAME')}</p>
                   </div>
                   <div className={styles.title}>
-                    <div>{t(`RESOURCES_MACHINE_${obj.phase.toUpperCase()}`)}</div>
+                    <div>
+                      {t(`RESOURCES_MACHINE_${obj.phase.toUpperCase()}`)}
+                    </div>
                     <p>{t('RESOURCES_DEPLOY_STEP')}</p>
                   </div>
                   <div className={styles.title}>
@@ -213,7 +219,11 @@ const DetailKaasList = props => {
                     <Text
                       // key='Memory'
                       // icon='memory'
-                      title={obj.ready_status ? t('RESOURCES_CLUSTER_READY') : t('RESOURCES_CLUSTER_NOT_READY')}
+                      title={
+                        obj.ready_status
+                          ? t('RESOURCES_CLUSTER_READY')
+                          : t('RESOURCES_CLUSTER_NOT_READY')
+                      }
                       description={t('RESOURCES_STATE')}
                     />
                   </div>
@@ -264,7 +274,11 @@ const DetailKaasList = props => {
                     <Text
                       // key='Memory'
                       // icon='memory'
-                      title={obj.ready_status ? t('RESOURCES_CLUSTER_READY') : t('RESOURCES_CLUSTER_NOT_READY')}
+                      title={
+                        obj.ready_status
+                          ? t('RESOURCES_CLUSTER_READY')
+                          : t('RESOURCES_CLUSTER_NOT_READY')
+                      }
                       description={t('RESOURCES_STATE')}
                     />
                   </div>
--- src/pages/clusters/containers/Resources/components/DetailMachineList/index.jsx
+++ src/pages/clusters/containers/Resources/components/DetailMachineList/index.jsx
@@ -1,4 +1,4 @@
-import { get } from 'lodash'
+import { get, isEmpty } from 'lodash'
 import React, { Fragment, useEffect, useState } from 'react'
 import { inject } from 'mobx-react'
 import classnames from 'classnames'
@@ -13,9 +13,13 @@ import {
   Loading,
   Pagination,
 } from '@kube-design/components'
+import { TinyArea } from 'components/Charts'
 import { Indicator, Panel, Text } from 'components/Base'
+import { getAreaChartOps } from 'utils/monitoring'
 
 import ContainerResourceStore from 'stores/resources/containerresource'
+import CustomStore from 'stores/monitoring/custom/monitor'
+import { getLocalTime } from 'utils'
 import styles from './index.scss'
 
 const DetailMachineList = props => {
@@ -26,6 +30,7 @@ const DetailMachineList = props => {
   // }
 
   const kaasStore = new ContainerResourceStore()
+  const customStore = new CustomStore()
 
   const cluster = props.detailStore?.detail.cluster
 
@@ -34,6 +39,22 @@ const DetailMachineList = props => {
 
   const [isLoading, setIsLoading] = useState(true)
   const [isSearchFlag, setIsSearchFlag] = useState(false)
+
+  const step = '5m'
+  const times = 100
+  const [machineData, setMachineData] = useState({
+    cpuData: [],
+    memoryData: [],
+  })
+  let timer = 0
+
+  useEffect(() => {
+    fetchData(0)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   const perPage = 6
   const [currentPage, setCurrentPage] = useState(1)
@@ -64,8 +85,8 @@ const DetailMachineList = props => {
       machineSearchData.length > 0
         ? getSliceData(machineSearchData, page)
         : params.name !== '' && params.name !== undefined
-          ? getSliceData(machineSearchData, page)
-          : getSliceData(filteredMachineList, page)
+        ? getSliceData(machineSearchData, page)
+        : getSliceData(filteredMachineList, page)
 
     setCurrentPage(page)
     setMachineSearchDataList(machineSearchData)
@@ -74,11 +95,132 @@ const DetailMachineList = props => {
     setIsLoading(false)
   }
 
+  const fetchData = timerSec => {
+    timer = setTimeout(async () => {
+      const kaasCpuFilteredData = []
+      const kaasMemoryFilteredData = []
+
+      Promise.all([getCpuUsageData(), getMemoryUsageData()]).then(values => {
+        if (values[0].length > 0) {
+          // eslint-disable-next-line array-callback-return
+          values[0].map(obj => {
+            if (obj.metric.pod.indexOf(props.name) === 0) {
+              kaasCpuFilteredData.push(obj)
+            }
+          })
+        }
+
+        if (values[1].length > 0) {
+          // eslint-disable-next-line array-callback-return
+          values[1].map(obj => {
+            if (obj.metric.pod.indexOf(props.name) === 0) {
+              kaasMemoryFilteredData.push(obj)
+            }
+          })
+        }
+
+        setMachineData({
+          ...machineData,
+          cpuData: kaasCpuFilteredData,
+          memoryData: kaasMemoryFilteredData,
+        })
+      })
+      fetchData(5000)
+    }, timerSec)
+  }
+
+  // kaas cpu data
+  const getCpuUsageData = () => {
+    const currentTime = Math.floor(Date.now() / 1000)
+    return new Promise(resolve => {
+      const cpuFetchData = customStore.fetchMetric({
+        expr: `(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle"}[${step}])) * ${times})) / 100`,
+        start: currentTime - 30000,
+        end: currentTime,
+        cluster,
+      })
+      resolve(cpuFetchData)
+    })
+  }
+
+  // kaas memory data
+  const getMemoryUsageData = () => {
+    const currentTime = Math.floor(Date.now() / 1000)
+    return new Promise(resolve => {
+      const memoryFetchData = customStore.fetchMetric({
+        expr: `node_memory_MemTotal_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*"}-node_memory_MemFree_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*"}-node_memory_Cached_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*"}`,
+        start: currentTime - 30000,
+        end: currentTime,
+        cluster,
+      })
+      resolve(memoryFetchData)
+    })
+  }
+
+  const getMonitoringCfgs = metrics => [
+    {
+      type: 'cpu',
+      title: 'CPU',
+      unitType: 'cpu',
+      legend: ['USED'],
+      data: [metrics.cpu],
+      bgColor: 'transparent',
+    },
+    {
+      type: 'memory',
+      title: 'MEMORY',
+      unitType: 'memory',
+      legend: ['USED'],
+      data: [metrics.memory],
+      bgColor: 'transparent',
+    },
+  ]
+
+  // eslint-disable-next-line no-shadow
+  const renderMonitorings = (nodeName, nodeNetwork) => {
+    const loading = isLoading
+    const podName = machineData.memoryData?.find(
+      obj => obj.metric?.instance?.split(':')[0] === nodeNetwork?.ip
+    )?.metric?.pod
+    const metrics = {
+      cpu: machineData.cpuData.find(obj => obj.metric.pod === podName),
+      memory: machineData.memoryData.find(obj => obj.metric.pod === podName),
+    }
+
+    if (loading) return <div className={styles.monitors}>{t('LOADING')}</div>
+
+    if (isEmpty(metrics.cpu) && isEmpty(metrics.memory))
+      return <div className={styles.monitors}>{t('NO_MONITORING_DATA')}</div>
+
+    const configs = getMonitoringCfgs(metrics)
+
+    return (
+      <div className={styles.monitors}>
+        <div className={styles.charts}>
+          {configs.map(item => {
+            const config = getAreaChartOps(item)
+
+            return (
+              <div key={item.type}>
+                <TinyArea
+                  key={item.type}
+                  width="100%"
+                  height={40}
+                  {...config}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
   const renderContent = () => {
     const content = machineSliceDataList.map((obj, index) => {
       return (
         <div className={styles.wrapper} key={index}>
-          <div className={classnames(styles.item)}>
+          <div className={styles.itemMain}>
             <div className={styles.icon}>
               <Icon name="nodes" size={40} />
               <Indicator
@@ -87,44 +229,41 @@ const DetailMachineList = props => {
                 flicker
               />
             </div>
-            <div className={classnames(styles.title, styles.name)}>
-              <div>{obj.name}</div>
-              <p>{t('RESOURCES_NAME')}</p>
-            </div>
-            <div className={styles.title}>
-              <Text
-                title={obj.ready_status ? t('RESOURCES_CLUSTER_READY') : t('RESOURCES_CLUSTER_NOT_READY')}
-                description={t('RESOURCES_NODE_STATUS')}
-              />
-            </div>
-            <div className={styles.title}>
-              <div>{t(`RESOURCES_MACHINE_${obj.phase.toUpperCase()}`)}</div>
-              <p>{t('RESOURCES_INFRA_STATUS')}</p>
-            </div>
-            <div className={styles.title}>
-              <div>
-                <Link
-                  className={styles.title}
-                  to={`/clusters/${cluster}/containerResource/${obj.cluster}`}
-                >
-                  {obj.cluster}
-                </Link>
+            <div className={styles.content}>
+              <div className={styles.text} style={{ width: '20%' }}>
+                <div>{obj.name}</div>
+                <p>
+                  {getLocalTime(obj.timestamp).format('YYYY-MM-DD HH:mm:ss')}
+                  {t('RESOURCES_CREATED')}
+                </p>
               </div>
-              <p>{t('RESOURCES_CLUSTER')}</p>
-            </div>
-            <div className={styles.title}>
-              <Text
-                title={obj.flavor}
-                description={t('Flavor')}
-              />
-            </div>
-            <div className={styles.title}>
-              <Text
-                title={obj.networks
-                  .filter(network => network.name !== 'k8s-pod-network')
-                  .map(o => `${o.ip} (${o.name})`)}
-                description={`IP (${t('RESOURCES_NETWORK')})`}
-              />
+              <div className={styles.text} style={{ width: '10%' }}>
+                <div>{t(`RESOURCES_MACHINE_${obj.phase.toUpperCase()}`)}</div>
+                <p>{t('RESOURCES_STATE')}</p>
+              </div>
+              <div className={styles.text} style={{ width: '10%' }}>
+                <div>
+                  <Link
+                    className={styles.title}
+                    to={`/clusters/${cluster}/containerResource/${obj.cluster}`}
+                  >
+                    {obj.cluster}
+                  </Link>
+                </div>
+                <p>{t('RESOURCES_CLUSTER')}</p>
+              </div>
+              <div className={styles.text}>
+                <Text
+                  title={obj.networks
+                    .filter(network => network.name !== 'k8s-pod-network')
+                    .map(o => `${o.ip} (${o.name})`)}
+                  description={`${t('RESOURCES_NETWORK')}`}
+                />
+              </div>
+              {renderMonitorings(
+                obj.name,
+                obj.networks.find(net => net.name === 'k8s-pod-network')
+              )}
             </div>
           </div>
         </div>
