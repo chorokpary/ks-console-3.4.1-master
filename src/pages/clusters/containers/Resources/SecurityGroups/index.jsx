92043f6bbcb2ab303fc75978fa7a4e7a49c3627f
changjjjjjjj <light9632@gmail.com>
2025-02-11 17:28:34 +0900

    Feat: prevent default security group edit & removal

--- src/pages/clusters/containers/Resources/SecurityGroups/index.jsx
+++ src/pages/clusters/containers/Resources/SecurityGroups/index.jsx
@@ -19,177 +19,167 @@ import ResourceTable from 'clusters/components/ResourceTable'
 
 import { Link } from 'react-router-dom'
 import React from 'react'
-import { toJS } from 'mobx'
-import { Avatar, Status } from 'components/Base'
+import { Avatar } from 'components/Base'
 import Banner from 'components/Cards/Banner'
-import withList, { ListPage, withClusterList } from 'components/HOCs/withList'
-import Table from 'components/Tables/List'
+import { ListPage, withClusterList } from 'components/HOCs/withList'
 
 import { getLocalTime } from 'utils'
-import { ICON_TYPES } from 'utils/constants'
 
-import RoleStore from 'stores/role'
 import SecurityGroupStore from 'stores/resources/securityGroups'
-import * as common from 'utils/resources'
-
-
 
 @withClusterList({
-    store: new SecurityGroupStore(),
-    module: 'security_groups',
-    authKey: 'securityGroups',
-    name: t('RESOURCES_SECURITY_GROUP'),
-    rowKey: 'id'
+  store: new SecurityGroupStore(),
+  module: 'security_groups',
+  authKey: 'securityGroups',
+  name: t('RESOURCES_SECURITY_GROUP'),
+  rowKey: 'id',
 })
 export default class SecurityGroups extends React.Component {
+  showAction(record) {
+    return !(
+      record.project === 'default' &&
+      (record.name === 'default' || record.name === 'ingress-all')
+    )
+  }
 
-    showAction(record) {
-        return globals.user.username !== record.name
-    }
+  get itemActions() {
+    const { getData, trigger } = this.props
+    return [
+      {
+        key: 'delete',
+        icon: 'trash',
+        text: t('RESOURCES_DELETE'),
+        action: 'delete',
+        show: this.showAction,
+        onClick: item =>
+          trigger('securityGroup.remove', {
+            detail: item,
+            success: getData,
+            ...this.props.match.params,
+          }),
+      },
+    ]
+  }
 
-    get itemActions() {
-        const { getData, trigger } = this.props
-        return [
-            {
-                key: 'delete',
-                icon: 'trash',
-                text: t('RESOURCES_DELETE'),
-                action: 'delete',
-                show: this.showAction,
-                onClick: item =>
-                    trigger('securityGroup.remove', {
-                        detail: item,
-                        success: getData,
-                        ...this.props.match.params,
-                    }),
-            },
-        ]
-    }
-
-    get tableActions() {
-        const { trigger, getData, routing, tableProps } = this.props
-        return {
-            ...tableProps.tableActions,
-            actions: [
-                {
-                    key: 'regist',
-                    type: 'control',
-                    text: t('RESOURCES_CREATE'),
-                    action: 'create',
-                    onClick: () =>
-                        trigger('securityGroup.regist', {
-                            ...this.props.match.params,
-                            type: this.name,
-                            success: getData,
-                        }),
-                },
-            ],
-            selectActions: [
-                {
-                    key: 'delete',
-                    type: 'danger',
-                    text: t('RESOURCES_DELETE'),
-                    action: 'delete',
-                    onClick: () =>
-                        trigger('securityGroup.remove.batch', {
-                            success: getData,
-                            ...this.props.match.params,
-                        }),
-                },
-            ],
-            getCheckboxProps: record => ({
-                disabled: !this.showAction(record),
-                name: record.name,
+  get tableActions() {
+    const { trigger, getData, tableProps } = this.props
+    return {
+      ...tableProps.tableActions,
+      actions: [
+        {
+          key: 'regist',
+          type: 'control',
+          text: t('RESOURCES_CREATE'),
+          action: 'create',
+          onClick: () =>
+            trigger('securityGroup.regist', {
+              ...this.props.match.params,
+              type: this.name,
+              success: getData,
             }),
-        }
+        },
+      ],
+      selectActions: [
+        {
+          key: 'delete',
+          type: 'danger',
+          text: t('RESOURCES_DELETE'),
+          action: 'delete',
+          onClick: () =>
+            trigger('securityGroup.remove.batch', {
+              success: getData,
+              ...this.props.match.params,
+            }),
+        },
+      ],
+      getCheckboxProps: record => ({
+        disabled: !this.showAction(record),
+        name: record.name,
+      }),
     }
+  }
 
-    getColumns = () => {
-        const { getSortOrder } = this.props
-        const { cluster } = this.props.match.params
-        return [
-            {
-                title: t('NAME'),
-                dataIndex: 'name',
-                sorter: true,
-                search: true,
-                render: (name, item) => (
-                    <Avatar
-                        icon="shield"
-                        iconSize={40}
-                        to={`/clusters/${cluster}/securityGroups/${name}/${item.id}`}
-                        title={name}
-                    />
-                ),
-            },
-            {
-                title: t('PROJECT'),
-                dataIndex: 'project',
-                isHideable: true,
-                width: 'auto',
-                render: project => (
-                    <Link to={`/clusters/${cluster}/projects/${project}/overview`}>
-                        {project}
-                    </Link>
-                ),
-            },
-            {
-                title: t('RESOURCES_INBOUND_RULE_COUNT'),
-                dataIndex: 'ingress_count',
-                isHideable: true,
-                width: 'auto',
-            },
-            {
-                title: t('RESOURCES_OUTBOUND_RULE_COUNT'),
-                dataIndex: 'egress_count',
-                isHideable: true,
-                width: 'auto',
-            },
-            {
-                title: t('RESOURCES_REGIST_DATE'),
-                dataIndex: 'timestamp',
-                isHideable: true,
-                sorter: true,
-                sortOrder: getSortOrder('descend'),
-                width: 150,
-                render: date => (
-                    <p>
-                        {date
-                            ? getLocalTime(date).format('YYYY-MM-DD HH:mm:ss')
-                            : t('-')}
-                    </p>
-                ),
-            },
-        ]
-    }
+  getColumns = () => {
+    const { getSortOrder } = this.props
+    const { cluster } = this.props.match.params
+    return [
+      {
+        title: t('NAME'),
+        dataIndex: 'name',
+        sorter: true,
+        search: true,
+        render: (name, item) => (
+          <Avatar
+            icon="shield"
+            iconSize={40}
+            to={`/clusters/${cluster}/securityGroups/${name}/${item.id}`}
+            title={name}
+          />
+        ),
+      },
+      {
+        title: t('PROJECT'),
+        dataIndex: 'project',
+        isHideable: true,
+        width: 'auto',
+        render: project => (
+          <Link to={`/clusters/${cluster}/projects/${project}/overview`}>
+            {project}
+          </Link>
+        ),
+      },
+      {
+        title: t('RESOURCES_INBOUND_RULE_COUNT'),
+        dataIndex: 'ingress_count',
+        isHideable: true,
+        width: 'auto',
+      },
+      {
+        title: t('RESOURCES_OUTBOUND_RULE_COUNT'),
+        dataIndex: 'egress_count',
+        isHideable: true,
+        width: 'auto',
+      },
+      {
+        title: t('RESOURCES_REGIST_DATE'),
+        dataIndex: 'timestamp',
+        isHideable: true,
+        sorter: true,
+        sortOrder: getSortOrder('descend'),
+        width: 150,
+        render: date => (
+          <p>
+            {date ? getLocalTime(date).format('YYYY-MM-DD HH:mm:ss') : t('-')}
+          </p>
+        ),
+      },
+    ]
+  }
 
-    get emptyProps() {
-        return { desc: t('RESOURCES_NO_DATA') }
-    }
+  get emptyProps() {
+    return { desc: t('RESOURCES_NO_DATA') }
+  }
 
-    render() {
-
-        const { bannerProps, tableProps } = this.props
-        return (
-            <ListPage {...this.props}>
-                <Banner
-                    icon="shield"
-                    {...bannerProps}
-                    tabs={this.tabs}
-                    title={t('RESOURCES_SECURITY_GROUP')}
-                    description={t('RESOURCES_SECURITY_GROUP_DESC')}
-                />
-                <ResourceTable
-                    {...tableProps}
-                    emptyProps={this.emptyProps}
-                    tableActions={this.tableActions}
-                    itemActions={this.itemActions}
-                    columns={this.getColumns()}
-                    columnSearch={this.columnSearch}
-                />
-            </ListPage>
-        )
-    }
+  render() {
+    const { bannerProps, tableProps } = this.props
+    return (
+      <ListPage {...this.props}>
+        <Banner
+          icon="shield"
+          {...bannerProps}
+          tabs={this.tabs}
+          title={t('RESOURCES_SECURITY_GROUP')}
+          description={t('RESOURCES_SECURITY_GROUP_DESC')}
+        />
+        <ResourceTable
+          {...tableProps}
+          emptyProps={this.emptyProps}
+          tableActions={this.tableActions}
+          itemActions={this.itemActions}
+          columns={this.getColumns()}
+          columnSearch={this.columnSearch}
+        />
+      </ListPage>
+    )
+  }
 }
-
-
