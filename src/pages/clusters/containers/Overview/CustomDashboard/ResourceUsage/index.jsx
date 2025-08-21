45103320627be6ed1cf89e3e9c1bcabbc75c6c83
ked <ked@ked-MacBook-Pro.local>
2025-08-21 09:38:55 +0900

    fix: dashboard 속도 개선

--- src/pages/clusters/containers/Overview/CustomDashboard/ResourceUsage/index.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/ResourceUsage/index.jsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Loading } from '@kube-design/components'
-import { getLastMonitoringData, getAreaChartOps, getSuitableUnit, getValueByUnit } from 'utils/monitoring'
+import {
+  getLastMonitoringData,
+  getAreaChartOps,
+  getSuitableUnit,
+  getValueByUnit,
+} from 'utils/monitoring'
 import { get, last } from 'lodash'
 import { SimpleArea } from 'components/Charts'
 import VmStore from 'stores/resources/vms'
@@ -23,31 +28,31 @@ const MetricTypes = {
   // pod_capacity: 'cluster_pod_quota',
   pod_utilisation: 'cluster_pod_utilisation',
   pod_cpu_usage: 'pod_cpu_usage',
-  pod_memory_usage: 'pod_memory_usage'
+  pod_memory_usage: 'pod_memory_usage',
 }
 
 const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
-  const podStore = new PodStore();
-  const customStore = new CustomStore();
-  const vmStore = new VmStore();
-  const resourceStore = new ResourceStore();
+  const podStore = new PodStore()
+  const customStore = new CustomStore()
+  const vmStore = new VmStore()
+  const resourceStore = new ResourceStore()
 
-  const [tabData, setTabData] = useState();
-  const [tabActive, setTabActive] = useState('cpu');
-  const [tabContentData, setTabContentData] = useState([]);
-  const [tabContent, setTabContent] = useState();
-  const [tabContentActive, setTabContentActive] = useState(false);
+  const [tabData, setTabData] = useState()
+  const [tabActive, setTabActive] = useState('cpu')
+  const [tabContentData, setTabContentData] = useState([])
+  const [tabContent, setTabContent] = useState()
+  const [tabContentActive, setTabContentActive] = useState(false)
   const [rightTab, setRightTab] = useState('node')
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
-  const [metricData, setMetricData] = useState([]);
-  const [vmData, setVmData] = useState({ cpuData: [], memoryData: [] });
-  const [kaasData, setKaasData] = useState({ cpuData: [], memoryData: [] });
-  const [podData, setPodData] = useState([]);
+  const [metricData, setMetricData] = useState([])
+  const [vmData, setVmData] = useState({ cpuData: [], memoryData: [] })
+  const [kaasData, setKaasData] = useState({ cpuData: [], memoryData: [] })
+  const [podData, setPodData] = useState([])
+  const podFetchedRef = useRef(false)
 
   useEffect(() => {
-
-    let cleanupTrigger = true;
+    let cleanupTrigger = true
     const getData = async () => {
       setLoading(true)
 
@@ -58,51 +63,43 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
         times: 100,
         // step: '3600s', // 최근 7일
         // times: 160,
-        cluster: props.cluster
+        cluster: props.cluster,
       })
 
       // first render
-      handleData('node', metricData);
-      handleContenOption('node', metricData);
+      handleData('node', metricData)
+      handleContenOption('node', metricData)
       setMetricData(metricData)
-
-      // pod data
-      const podData = await podStore.fetchMetrics({
-        metrics: Object.values(MetricTypes),
-        step: '5m',
-        times: 100,
-        cluster: props.cluster
-      })
-      handlePodData(podData)
 
       // vm list
       const vmList = await vmStore.vmList({ limit: -1, ...props })
-      let promsql_pod_vm_list = '';
-      let vm_list_length = 0;
+      let promsql_pod_vm_list = ''
+      let vm_list_length = 0
       vmList.map(obj => {
-        promsql_pod_vm_list = promsql_pod_vm_list + obj.id + "|"
-        vm_list_length++;
+        promsql_pod_vm_list = promsql_pod_vm_list + obj.id + '|'
+        vm_list_length++
       })
 
       // kaas list
       const kaasList = await resourceStore.fetchList({ limit: 1000, ...props })
-      let promsql_pod_kaas_list = ""
-      let kaas_list_length = 0;
-      kaasList.map((obj) => {
+      let promsql_pod_kaas_list = ''
+      let kaas_list_length = 0
+      kaasList.map(obj => {
         const kaasName = get(obj, 'name')
-        promsql_pod_kaas_list += promsql_pod_kaas_list != "" ? ("|" + kaasName + '.*') : kaasName + '.*';
-        kaas_list_length++;
+        promsql_pod_kaas_list +=
+          promsql_pod_kaas_list != '' ? '|' + kaasName + '.*' : kaasName + '.*'
+        kaas_list_length++
       })
 
       // vm cpu data
       const step = '5m'
       const times = 100
-      var currentTime = Math.floor(Date.now() / 1000);
+      var currentTime = Math.floor(Date.now() / 1000)
       const vmCpuData = await customStore.fetchMetric({
         expr: `sum(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle",pod=~"${promsql_pod_vm_list}"}[${step}])) * ${times})) / 100 / ${vm_list_length}`,
         start: currentTime - 30000,
         end: currentTime,
-        cluster: props.cluster
+        cluster: props.cluster,
       })
 
       // vm memory data
@@ -110,16 +107,20 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
         expr: `sum(node_memory_MemTotal_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*",pod=~"${promsql_pod_vm_list}"}-node_memory_MemFree_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*",pod=~"${promsql_pod_vm_list}"}-node_memory_Cached_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*",pod=~"${promsql_pod_vm_list}"}) / ${vm_list_length}`,
         start: currentTime - 30000,
         end: currentTime,
-        cluster: props.cluster
+        cluster: props.cluster,
       })
-      setVmData({ ...vmData, ['cpuData']: vmCpuData, ['memoryData']: vmMemoryData })
+      setVmData({
+        ...vmData,
+        ['cpuData']: vmCpuData,
+        ['memoryData']: vmMemoryData,
+      })
 
       // kaas cpu data
       const kaasCpuData = await customStore.fetchMetric({
         expr: `sum(100 - (avg by (pod) (irate(node_cpu_seconds_total{namespace="default",service="launcher-node-exporter",mode="idle",pod!~"${promsql_pod_kaas_list}"}[${step}])) * ${times})) / 100 / ${kaas_list_length}`,
         start: currentTime - 30000,
         end: currentTime,
-        cluster: props.cluster
+        cluster: props.cluster,
       })
 
       // kaas memory data
@@ -127,40 +128,50 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
         expr: `sum(node_memory_MemTotal_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*",pod!~"${promsql_pod_kaas_list}"}-node_memory_MemFree_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*",pod!~"${promsql_pod_kaas_list}"}-node_memory_Cached_bytes{service='launcher-node-exporter',pod!~"virt-launcher-.*",pod!~"${promsql_pod_kaas_list}"})/ ${kaas_list_length}`,
         start: currentTime - 30000,
         end: currentTime,
-        cluster: props.cluster
+        cluster: props.cluster,
       })
-      setKaasData({ ...kaasData, ['cpuData']: kaasCpuData, ['memoryData']: kaasMemoryData })
+      setKaasData({
+        ...kaasData,
+        ['cpuData']: kaasCpuData,
+        ['memoryData']: kaasMemoryData,
+      })
 
       if (cleanupTrigger) {
         setLoading(false)
       }
-    };
-    getData();
+    }
+    getData()
     return () => {
       cleanupTrigger = false
       setLoading(false)
     }
-
   }, [])
 
-  const handlePodData = (podData) => {
+  const handlePodData = podData => {
     const data = {
-      'pod_cpu_usage': [{ values: sumPodDataValue(get(podData, `pod_cpu_usage.data.result`)) }],
-      'pod_memory_usage': [{ values: sumPodDataValue(get(podData, `pod_memory_usage.data.result`)) }],
+      pod_cpu_usage: [
+        { values: sumPodDataValue(get(podData, `pod_cpu_usage.data.result`)) },
+      ],
+      pod_memory_usage: [
+        {
+          values: sumPodDataValue(get(podData, `pod_memory_usage.data.result`)),
+        },
+      ],
     }
     setPodData(data)
+    return data
   }
-  const sumPodDataValue = (_podData) => {
-    const _values = _podData?.map(obj => (obj.values))
-    let valueArr = [];
+  const sumPodDataValue = _podData => {
+    const _values = _podData?.map(obj => obj.values)
+    let valueArr = []
     _values?.map((arr, idx) => {
-      let a = 0;
-      arr.map((arr2) => {
-        a += Number(arr2[1]);
+      let a = 0
+      arr.map(arr2 => {
+        a += Number(arr2[1])
       })
-      valueArr.push([arr[idx]?.[0], a / arr.length]);
+      valueArr.push([arr[idx]?.[0], a / arr.length])
     })
-    return valueArr;
+    return valueArr
   }
 
   // handle left data
@@ -174,27 +185,42 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
   }
 
   // left tab active
-  const onClickLeftTab = (activeTab) => {
+  const onClickLeftTab = activeTab => {
     setTabActive(activeTab)
     setTabContent(tabContentData.filter(obj => obj.activeTab == activeTab)[0])
   }
 
   // right tab active
-  const onClickRightTab = (tab) => {
-    let data;
+  const onClickRightTab = async tab => {
+    setRightTab(tab)
+    let data
     if (tab === 'node') {
-      data = metricData;
+      data = metricData
     } else if (tab === 'pod') {
-      data = podData;
+      if (!podFetchedRef.current) {
+        setLoading(true)
+
+        const fetchedPodData = await podStore.fetchMetrics({
+          metrics: Object.values(MetricTypes),
+          step: '5m',
+          times: 100,
+          cluster: props.cluster,
+        })
+        const handleData = handlePodData(fetchedPodData)
+        podFetchedRef.current = true // 호출 기록
+        data = handleData
+        setLoading(false)
+      } else {
+        data = podData
+      }
     } else if (tab === 'vm') {
-      data = vmData;
+      data = vmData
     } else if (tab === 'kaas') {
-      data = kaasData;
+      data = kaasData
     }
     handleContenOption(tab, data)
     handleData(tab, data)
     setTabActive('cpu')
-    setRightTab(tab)
   }
 
   useEffect(() => {
@@ -210,7 +236,6 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
     }
   }, [loading])
 
-
   return (
     <>
       <div className="grid-stack-item" gs-x={x} gs-y={y} gs-w={w} gs-h={h}>
@@ -222,19 +247,44 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
               <div className="right">
                 <div className="dash_boxtab">
                   <label htmlFor="name2_1">
-                    <input type="radio" name="box-tab" id="name2_1" value="name3" defaultChecked onClick={() => onClickRightTab('node')} />
+                    <input
+                      type="radio"
+                      name="box-tab"
+                      id="name2_1"
+                      value="name3"
+                      defaultChecked
+                      onClick={() => onClickRightTab('node')}
+                    />
                     <span>{t('RESOURCES_NODE')}</span>
                   </label>
                   <label htmlFor="name2_2">
-                    <input type="radio" name="box-tab" id="name2_2" value="name4" onClick={() => onClickRightTab('pod')} />
+                    <input
+                      type="radio"
+                      name="box-tab"
+                      id="name2_2"
+                      value="name4"
+                      onClick={() => onClickRightTab('pod')}
+                    />
                     <span>{t('POD_PL')}</span>
                   </label>
                   <label htmlFor="name2_3">
-                    <input type="radio" name="box-tab" id="name2_3" value="name5" onClick={() => onClickRightTab('vm')} />
+                    <input
+                      type="radio"
+                      name="box-tab"
+                      id="name2_3"
+                      value="name5"
+                      onClick={() => onClickRightTab('vm')}
+                    />
                     <span>{t('RESOURCES_VM')}</span>
                   </label>
                   <label htmlFor="name2_4">
-                    <input type="radio" name="box-tab" id="name2_4" value="name6" onClick={() => onClickRightTab('kaas')} />
+                    <input
+                      type="radio"
+                      name="box-tab"
+                      id="name2_4"
+                      value="name6"
+                      onClick={() => onClickRightTab('kaas')}
+                    />
                     <span>KaaS</span>
                   </label>
                 </div>
@@ -244,38 +294,67 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
               <div className="grid_info style_chart">
                 <div className="box type_chart">
                   <div className="cont1">
-                    {tabData && tabData.map(data => (
-                      <div className={`chart_tab ${tabActive == data.activeTab ? 'on' : ''}`} key={data.name} onClick={() => onClickLeftTab(data.activeTab)}>
-                        <div className="title">
-                          <i className={`ico-type-${data.unitType} ${data.name}`}></i>
-                          <h5>{data.name}</h5>
-                        </div>
-                        {rightTab == 'node' ?
-                          <div className="data">
-                            <div className="number_wrap">
-                              <p><span className="em">{data._used}</span> / {data._total}<span className="unit" style={{ marginLeft: '3px' }}>{' '}{t(data._unit)}</span></p>
-                              <p>{Math.round(data._percent)}%</p>
-                            </div>
-                            <div className="graph_wrap">
-                              <div className="graph_bar">
-                                <div className="bar animate-bar" style={{ width: `${Math.round(data._percent)}%` }}></div>
+                    {tabData &&
+                      tabData.map(data => (
+                        <div
+                          className={`chart_tab ${
+                            tabActive == data.activeTab ? 'on' : ''
+                          }`}
+                          key={data.name}
+                          onClick={() => onClickLeftTab(data.activeTab)}
+                        >
+                          <div className="title">
+                            <i
+                              className={`ico-type-${data.unitType} ${data.name}`}
+                            ></i>
+                            <h5>{data.name}</h5>
+                          </div>
+                          {rightTab == 'node' ? (
+                            <div className="data">
+                              <div className="number_wrap">
+                                <p>
+                                  <span className="em">{data._used}</span> /{' '}
+                                  {data._total}
+                                  <span
+                                    className="unit"
+                                    style={{ marginLeft: '3px' }}
+                                  >
+                                    {' '}
+                                    {t(data._unit)}
+                                  </span>
+                                </p>
+                                <p>{Math.round(data._percent)}%</p>
+                              </div>
+                              <div className="graph_wrap">
+                                <div className="graph_bar">
+                                  <div
+                                    className="bar animate-bar"
+                                    style={{
+                                      width: `${Math.round(data._percent)}%`,
+                                    }}
+                                  ></div>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                          :
-                          <div className="data">
-                            <div className="number_wrap rgt">
-                              <p><span className="em">{isNaN(data._used) ? 0 : data._used}</span><span className="unit">{t(data._unit)}</span></p>
+                          ) : (
+                            <div className="data">
+                              <div className="number_wrap rgt">
+                                <p>
+                                  <span className="em">
+                                    {isNaN(data._used) ? 0 : data._used}
+                                  </span>
+                                  <span className="unit">{t(data._unit)}</span>
+                                </p>
+                              </div>
                             </div>
-                          </div>
-                        }
-                      </div>
-                    ))}
+                          )}
+                        </div>
+                      ))}
                   </div>
                   <div className="cont2">
-                    {tabContentActive &&
+                    {tabContentActive && (
                       <TabContent option={tabContent}></TabContent>
-                    }
+                    )}
                     {/* <div className="chart_01"></div> */}
                   </div>
                 </div>
@@ -284,7 +363,7 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
           </div>
           {/* // grid_item */}
         </div>
-      </div >
+      </div>
     </>
   )
 }
@@ -292,7 +371,6 @@ const ResourcesUsage = ({ monitorStore, x, y, w, h, ...props }) => {
 export default ResourcesUsage
 
 const TabContent = ({ option }) => {
-
   const commonProps = {
     key: option?.title,
     width: '100%',
@@ -302,13 +380,17 @@ const TabContent = ({ option }) => {
 
   return (
     <>
-      {config.data.length > 0 ?
-        <SimpleArea {...commonProps} {...config} style={{ padding: '10px', color: 'white' }} />
-        :
+      {config.data.length > 0 ? (
+        <SimpleArea
+          {...commonProps}
+          {...config}
+          style={{ padding: '10px', color: 'white' }}
+        />
+      ) : (
         <div className="grid_text">
           <span>{t('RESOURCES_NO_DATA')}</span>
         </div>
-      }
+      )}
     </>
   )
 }
