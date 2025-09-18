216f8a8d0e613f1784f8439c12a5e360b17975de
chorokpary <chorokpary@nate.com>
2025-09-18 10:19:26 +0900

    feat: GPU 노드 > 상세 페이지 그래프 및 GPU 모니터링  페이지 추가

---
+++ src/pages/clusters/containers/Resources/GpuNodes/Detail/GpuMonitoring/index.jsx
@@ -0,0 +1,259 @@
+import { isEmpty } from 'lodash'
+import React, { useState, useEffect } from 'react'
+import { observer, inject } from 'mobx-react'
+
+import { getChartData, getAreaChartOps, getCustomValue } from 'utils/monitoring'
+import CustomStore from 'stores/monitoring/custom/monitor'
+import VmStore from 'stores/resources/vms'
+
+import { Controller as MonitoringController } from 'components/Cards/Monitoring'
+import { SimpleArea } from 'components/Charts'
+
+const index = props => {
+  const store = props.detailStore
+  const customStore = new CustomStore()
+  const vmStore = new VmStore()  
+
+  const [vmDataList, setVmDataList] = useState([])
+  const [fetchParams, setFetchParams] = useState({})
+
+  const [vmGpuUtilData, setVmGpuUtilData] = useState([])
+  const [vmGpuRamData, setVmGpuRamData] = useState([])
+  const [vmGpuTempData, setVmGpuTempData] = useState([])
+  const [vmGpuPowerData, setVmGpuPowerData] = useState([])
+
+  const [vmGpuNvlinkData, setVmGpuNvlinkData] = useState([])
+
+  const [vmGpuInboundData, setVmGpuInboundData] = useState([])
+  const [vmGpuOutboundData, setVmGpuOutboundData] = useState([])
+
+  const getMinuteValue = (timeStr = '60s', hasUnit = true) => {
+    const unit = timeStr.slice(-1)
+    let value = parseFloat(timeStr)
+
+    switch (unit) {
+      default:
+      case 's':
+        break
+      case 'm':
+        value *= 60
+        break
+      case 'h':
+        value *= 60 * 60
+        break
+      case 'd':
+        value = value * 24 * 60 * 60
+        break
+    }
+    return hasUnit ? `${value}s` : value
+  }
+
+  const getTimeRange = ({ step = '600s', times = 20 } = {}) => {
+    const interval = parseFloat(step) * times
+    const end = Math.floor(Date.now() / 1000)
+    const start = Math.floor(end - interval)
+
+    return { start, end }
+  }
+
+  useEffect(() => {
+      fnGetData()
+    }, [])
+  
+  const fnGetData = async () => {
+    const params = {
+      cluster: store.detail.cluster,
+      limit: 10000,
+    }
+
+    const vmList = await vmStore.fetchList(params)
+    const vmData = vmList.filter(item => item.node === store.detail.name).map(item => item.name).join('|') || ''
+
+    setVmDataList(vmData)
+  }
+
+  const fetchData = async params => {
+    setFetchParams(params)
+    const paramsData = Object.assign(params, {
+      start: params.start,
+      end: params.end,
+      step: getMinuteValue(params.step),
+      times: params.times,
+    })
+
+    if (!paramsData.start || !paramsData.end) {
+      const timeRange = getTimeRange(paramsData)
+      paramsData.start = timeRange.start
+      paramsData.end = timeRange.end
+    }
+
+    const getVmGpuUtilData = async () => {
+      const gpuUtilDataExpr = `avg(DCGM_FI_DEV_GPU_UTIL{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"}) / 100`
+      const gpuUtilData = await customStore.fetchMetric({
+        expr: gpuUtilDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+
+      setVmGpuUtilData(gpuUtilData)
+    }
+
+    const getVmGpuRamData = async () => {
+      const gpuRamDataExpr = `avg(DCGM_FI_DEV_FB_USED{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"}) * 1000000`
+      const gpuRamData = await customStore.fetchMetric({
+        expr: gpuRamDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+
+      setVmGpuRamData(gpuRamData)
+    }
+
+    const getVmGpuPowerData = async () => {
+      const gpuPowerDataExpr = `avg(DCGM_FI_DEV_POWER_USAGE{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"})`
+
+      const gpuPowerData = await customStore.fetchMetric({
+        expr: gpuPowerDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+
+      setVmGpuPowerData(gpuPowerData)
+    }
+
+    const getVmGpuTempData = async () => {
+      const gpuTempDataExpr = `avg(DCGM_FI_DEV_GPU_TEMP{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"})`
+      const gpuTempData = await customStore.fetchMetric({
+        expr: gpuTempDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+
+      setVmGpuTempData(gpuTempData)
+    }
+
+    const getVmGpuNvlinkData = async () => {
+      const gpuNvlinkDataExpr = `sum(DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"}) * ${getCustomValue(
+        'bandwidthBytes',
+        'MBps'
+      )}`
+      const gpuNvlinkData = await customStore.fetchMetric({
+        expr: gpuNvlinkDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+      setVmGpuNvlinkData(gpuNvlinkData)
+    }
+
+    const getVmGpuInboundData = async () => {
+      const inboundLinuxDataExpr = `sum(rate(node_infiniband_port_data_received_bytes_total{job="launcher-node-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"}[2m]) * 8)`
+      const gpuInboundData = await customStore.fetchMetric({
+        expr: inboundLinuxDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+
+      setVmGpuInboundData(gpuInboundData)
+    }
+
+    const getVmGpuOutboundData = async () => {
+      const outboundLinuxDataExpr = `sum(rate(node_infiniband_port_data_transmitted_bytes_total{job="launcher-node-exporter", pod=~"${vmDataList}", namespace="${store.detail.cluster}"}[2m]) * 8)`
+      const gpuOutboundData = await customStore.fetchMetric({
+        expr: outboundLinuxDataExpr,
+        ...paramsData,
+        cluster: props.match.params.cluster,
+      })
+
+      setVmGpuOutboundData(gpuOutboundData)
+    }
+
+    getVmGpuUtilData()
+    getVmGpuRamData()
+    getVmGpuTempData()
+    getVmGpuPowerData()
+    getVmGpuNvlinkData()
+    getVmGpuInboundData()
+    getVmGpuOutboundData()
+  }
+
+  const getMonitoringCfgs = () => {
+    return [
+       {
+        type: 'utilisation',
+        title: 'RESOURCES_GPU_UTILIZATION',
+        unit: '%',
+        legend: ['RESOURCES_GPU_UTILIZATION'],
+        data: vmGpuUtilData,
+      },
+      {
+        type: 'utilisation',
+        title: 'RESOURCES_GPU_RAM_USAGE',
+        unit: '%',
+        unitType: 'memory',
+        legend: ['RESOURCES_GPU_UTILIZATION'],
+        data: vmGpuRamData,
+      },
+      {
+        type: 'utilisation',
+        title: t('RESOURCES_GPU_TEMPERATURE'),
+        unit: '°C',
+        legend: ['RESOURCES_GPU_TEMPERATURE'],
+        data: vmGpuTempData,
+      },
+      {
+        type: 'utilisation',
+        title: t('RESOURCES_GPU_POWER'),
+        unit: 'W',
+        legend: ['RESOURCES_GPU_POWER'],
+        data: vmGpuPowerData,
+      },
+      {
+        type: 'bandwidth',
+        title: 'IB ' + t('RESOURCES_INBOUND'),
+        unitType: 'bandwidth',
+        legend: ['IB ' + t('RESOURCES_INBOUND')],
+        data: vmGpuInboundData,
+      },
+      {
+        type: 'bandwidth',
+        title: 'IB ' + t('RESOURCES_OUTBOUND'),
+        unitType: 'bandwidth',
+        legend: ['IB ' + t('RESOURCES_OUTBOUND')],
+        data: vmGpuOutboundData,
+      },
+      {
+        type: 'bandwidth',
+        title: 'NVLink ' + t('TRAFFIC'),
+        unitType: 'bandwidthBytes',
+        legend: ['NVLink ' + t('TRAFFIC')],
+        data: vmGpuNvlinkData,
+      },
+    ]
+  }
+
+  useEffect(() => {
+    if (vmDataList) {
+      fetchData({ ...fetchParams })
+    }
+  }, [vmDataList])  
+
+  const { isLoading, isRefreshing } = customStore
+  const configs = getMonitoringCfgs()
+
+  return (
+    <MonitoringController
+      title={t('RESOURCES_GPU_MONITORING')}
+      onFetch={fetchData}
+      loading={isLoading}
+      refreshing={isRefreshing}
+    >
+      {configs.map(item => {
+        const config = getAreaChartOps(item)
+        if (isEmpty(config.data)) return null
+        return <SimpleArea key={config.title} width="100%" {...config} />
+      })}
+    </MonitoringController>
+  )
+}
+
+export default inject('detailStore')(observer(index))
