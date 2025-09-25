e28d7e35455055778390ffac0b7cba3b55d08b88
chorokpary <chorokpary@nate.com>
2025-09-25 09:27:09 +0900

    fix: GPU 노드 > 상세 페이지 리소스 사용량 관련 수정

--- src/pages/clusters/containers/Resources/GpuNodes/Detail/Status/index.jsx
+++ src/pages/clusters/containers/Resources/GpuNodes/Detail/Status/index.jsx
@@ -19,20 +19,15 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 
-import { toJS } from 'mobx'
-import { get, isEmpty, flatten } from 'lodash'
-import { getSuitableUnit, getCustomValue } from 'utils/monitoring'
-
 import { Panel } from 'components/Base'
-import MonitorTab from 'components/Cards/Monitoring/MonitorTab'
 import DeploymentCard from './DeploymentCard'
 import DetailGpuDeviceList from 'pages/clusters/containers/Resources/components/DetailGpuDeviceList';
+import DetailGpuResource from 'pages/clusters/containers/Resources/components/DetailGpuResource'
 
-import NodeMonitoringStore from 'stores/monitoring/node'
 import VmStore from 'stores/resources/vms'
-import CustomStore from 'stores/monitoring/custom/monitor'
 
 import styles from './index.scss'
+import { join } from 'lodash';
 
 @inject('detailStore')
 @observer
@@ -41,60 +36,15 @@ export default class Status extends React.Component {
     super(props)
 
     this.store = props.detailStore
-    this.monitoringStore = new NodeMonitoringStore({ cluster: this.cluster })
     this.vmStore = new VmStore({ cluster: this.cluster })
-    this.customStore = new CustomStore({ cluster: this.cluster })
-    
+
     this.state = {
-      fetchParams: {},
-      vmDataList: [],     
-      vmGpuUtilData: null,
-      vmGpuRamData: null,
-      vmGpuPowerData: null,
-      vmGpuTempData: null,
-      vmGpuNvlinkData: null,
-      vmGpuInboundData: null,
-      vmGpuOutboundData: null,
+      vmList: '',     
     }
   }
 
   componentDidMount() {
     this.fnGetData()
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.vmDataList !== this.state.vmDataList) {
-      this.fetchDataGpu({ ...this.fetchParams });
-    }
-  }
-
-  getMinuteValue = (timeStr = '60s', hasUnit = true) => {
-    const unit = timeStr.slice(-1)
-    let value = parseFloat(timeStr)
-
-    switch (unit) {
-      default:
-      case 's':
-        break
-      case 'm':
-        value *= 60
-        break
-      case 'h':
-        value *= 60 * 60
-        break
-      case 'd':
-        value = value * 24 * 60 * 60
-        break
-    }
-    return hasUnit ? `${value}s` : value
-  }
-
-  getTimeRange = ({ step = '180s', times = 20 } = {}) => {
-    const interval = parseFloat(step) * times
-    const end = Math.floor(Date.now() / 1000)
-    const start = Math.floor(end - interval)
-
-    return { start, end }
   }
 
   fnGetData = async () => {
@@ -103,196 +53,12 @@ export default class Status extends React.Component {
       limit: 10000,
     }
 
-    const vmList = await this.vmStore.fetchList(params)
-    const vmData = vmList.filter(item => item.node === this.store.detail.name).map(item => item.name).join('|') || ''
+    const vmListData = await this.vmStore.fetchList(params)
+    const vmJoinData = vmListData.filter(item => item.node === this.store.detail.name).map(item => item.name).join('|') || ''
 
-    this.setState({ vmDataList: vmData });
+    this.setState({ vmList: vmJoinData });
   }
 
-  fetchDataGpu = async (params) => {
-    this.setState({ fetchParams: params });
-
-    const paramsData = Object.assign({}, params, {
-      start: params.start,
-      end: params.end,
-      step: this.getMinuteValue(params.step),
-      times: params.times,
-    });
-
-    if (!paramsData.start || !paramsData.end) {
-      const timeRange = this.getTimeRange(paramsData);
-      paramsData.start = timeRange.start;
-      paramsData.end = timeRange.end;
-    }
-
-    const { vmDataList } = this.state;
-
-    const getVmGpuUtilData = async () => {
-      const gpuUtilDataExpr = `avg(DCGM_FI_DEV_GPU_UTIL{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"}) / 100`;
-      const gpuUtilData = await this.customStore.fetchMetric({
-        expr: gpuUtilDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuUtilData: gpuUtilData });
-    };
-
-    const getVmGpuRamData = async () => {
-      const gpuRamDataExpr = `avg(DCGM_FI_DEV_FB_USED{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"}) * 1000000`;
-      const gpuRamData = await this.customStore.fetchMetric({
-        expr: gpuRamDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuRamData: gpuRamData });
-    };
-
-    const getVmGpuPowerData = async () => {
-      const gpuPowerDataExpr = `avg(DCGM_FI_DEV_POWER_USAGE{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"})`;
-      const gpuPowerData = await this.customStore.fetchMetric({
-        expr: gpuPowerDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuPowerData: gpuPowerData });
-    };
-
-    const getVmGpuTempData = async () => {
-      const gpuTempDataExpr = `avg(DCGM_FI_DEV_GPU_TEMP{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"})`;
-      const gpuTempData = await this.customStore.fetchMetric({
-        expr: gpuTempDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuTempData: gpuTempData });
-    };
-
-    const getVmGpuNvlinkData = async () => {
-      const gpuNvlinkDataExpr = `sum(DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL{job="launcher-dcgm-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"}) * ${getCustomValue(
-        'bandwidthBytes',
-        'MBps'
-      )}`;
-      const gpuNvlinkData = await this.customStore.fetchMetric({
-        expr: gpuNvlinkDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuNvlinkData: gpuNvlinkData });
-    };
-
-    const getVmGpuInboundData = async () => {
-      const inboundLinuxDataExpr = `sum(rate(node_infiniband_port_data_received_bytes_total{job="launcher-node-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"}[2m]) * 8)`;
-      const gpuInboundData = await this.customStore.fetchMetric({
-        expr: inboundLinuxDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuInboundData: gpuInboundData });
-    };
-
-    const getVmGpuOutboundData = async () => {
-      const outboundLinuxDataExpr = `sum(rate(node_infiniband_port_data_transmitted_bytes_total{job="launcher-node-exporter", pod=~"${vmDataList}", namespace="${this.store.detail.cluster}"}[2m]) * 8)`;
-      const gpuOutboundData = await this.customStore.fetchMetric({
-        expr: outboundLinuxDataExpr,
-        ...paramsData,
-        cluster: this.store.detail.cluster,
-      });
-      this.setState({ vmGpuOutboundData: gpuOutboundData });
-    };
-
-    getVmGpuUtilData();
-    getVmGpuRamData();
-    getVmGpuTempData();
-    getVmGpuPowerData();
-    getVmGpuNvlinkData();
-    getVmGpuInboundData();
-    getVmGpuOutboundData();
-  };
-
-  renderResourceStatus() {
-
-     const {   
-      vmGpuUtilData,
-      vmGpuRamData,
-      vmGpuPowerData,
-      vmGpuTempData,
-      vmGpuNvlinkData,
-      vmGpuInboundData,
-      vmGpuOutboundData, 
-    } = this.state;
-
-    return (
-      <Panel
-        className={styles.resources}
-        title={t('RESOURCE_USAGE')}
-        loading={vmGpuOutboundData ? false : true}
-      >
-        <MonitorTab
-          tabs={[
-            {
-              key: 'gpucluster',
-              icon: 'ico-type-hostgpu',
-              unit: '%',
-              legend: ['RESOURCES_GPU_UTILIZATION'],
-              title: 'RESOURCES_GPU_UTILIZATION',
-              data: vmGpuUtilData,
-            },
-            {
-              key: 'memory',
-              icon: 'memory',
-              unit: getSuitableUnit(flatten(vmGpuRamData?.map(result => get(result, 'values') || [])), 'memory'),
-              legend: ['RESOURCES_GPU_RAM_USAGE'],
-              title: 'RESOURCES_GPU_RAM_USAGE',
-              data: vmGpuRamData,
-            },            
-            {
-              key: 'temperature',
-              icon: 'ico-type-temperature',
-              unit: '°C',
-              legend: ['RESOURCES_GPU_TEMPERATURE'],
-              title: 'RESOURCES_GPU_TEMPERATURE',
-              data: vmGpuTempData,
-            },
-            {
-              key: 'power',
-              icon: 'ico-type-power',
-              unit: 'W',
-              legend: ['RESOURCES_GPU_POWER'],
-              title: 'RESOURCES_GPU_POWER',
-              data: vmGpuPowerData,
-            },
-            {
-              key: 'inbound',
-              icon: 'ico-type-inbound',
-              type: 'bandwidth',
-              unit: getSuitableUnit(flatten(vmGpuInboundData?.map(result => get(result, 'values') || [])), 'bandwidth'),
-              legend: ['RESOURCES_GPU_IB_INBOUND'],
-              title: 'RESOURCES_GPU_IB_INBOUND',
-              data: vmGpuInboundData,
-            },
-            {
-              key: 'outbound',
-              icon: 'ico-type-outbound',
-              type: 'bandwidth',
-              unit: getSuitableUnit(flatten(vmGpuOutboundData?.map(result => get(result, 'values') || [])), 'bandwidth'),
-              legend: ['RESOURCES_GPU_IB_OUTBOUND'],
-              title: 'RESOURCES_GPU_IB_OUTBOUND',
-              data: vmGpuOutboundData,
-            },
-            {
-              key: 'traffic',
-              icon: 'topology',
-              type: 'bandwidth',
-              unit: getSuitableUnit(flatten(vmGpuNvlinkData?.map(result => get(result, 'values') || [])), 'bandwidthBytes'),
-              legend: ['RESOURCES_GPU_NVLINK_TRAFFIC'],
-              title: 'RESOURCES_GPU_NVLINK_TRAFFIC',
-              data: vmGpuNvlinkData,
-            },
-          ]}
-        />
-      </Panel>
-    )
-  }
   renderDeployments() {
     const { deploy } = this.store.detail.gpunode
     const container_toolkit = { name: "container_toolkit", flag: deploy.container_toolkit }
@@ -342,8 +108,15 @@ export default class Status extends React.Component {
   render() {
     return (
       <div className={styles.main}>
-        {this.renderResourceStatus()}
-        {this.state.vmGpuOutboundData && this.renderDeployments()}
+        {this.state.vmList && 
+          <DetailGpuResource
+            {...this.props}
+            cluster={this.store.detail.cluster}
+            namespace={this.store.detail.cluster}
+            vmList={this.state.vmList}
+          />
+        }
+        {this.renderDeployments()}
       </div>
     )
   }
