d6510437a0ee5be97b5cd98deb62063498f49b68
ked <ked@ked-MacBook-Pro.local>
2025-10-30 09:05:56 +0900

    fix: gpu map tooltip script

--- src/pages/clusters/containers/Overview/CustomDashboard/GpuMap/Gpu/index.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/GpuMap/Gpu/index.jsx
@@ -12,6 +12,16 @@ const Gpu = ({
   vmList,
   ...props
 }) => {
+  useEffect(() => {
+    //gpu_tile .name 길어서 ellipsis 처리 될 때 tooltip 표현 (27자 이상일 때만)
+    document.querySelectorAll('.gpu_tile .name').forEach(el => {
+      const text = el.textContent.trim()
+      if (text.length > 27) {
+        el.setAttribute('data-text', text)
+      }
+    })
+  }, [])
+
   const [selectGpu, setSelectGpu] = useState({})
 
   return (
--- src/pages/clusters/containers/Overview/CustomDashboard/GpuMap/index.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/GpuMap/index.jsx
@@ -115,13 +115,6 @@ const GpuMap = ({ widgetKey, monitorStore, ...props }) => {
       }
     }
 
-    //gpu_tile .name 길어서 ellipsis 처리 될 때 tooltip 표현 (27자 이상일 때만)
-    document.querySelectorAll('.gpu_tile .name').forEach(el => {
-      const text = el.textContent.trim()
-      if (text.length > 27) {
-        el.setAttribute('data-text', text)
-      }
-    })
     document.addEventListener('click', handleClickOutside)
     return () => document.removeEventListener('click', handleClickOutside)
   }, [])
