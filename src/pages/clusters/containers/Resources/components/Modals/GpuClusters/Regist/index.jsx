27afdb1d7d72221105f7cfd2dc8d656100b2dfef
chorokpary <chorokpary@nate.com>
2025-12-03 17:47:43 +0900

    fix: jira-72 > GPU 클러스터 > 상세 > 가상머신 생성 > 가산머신 생성 수의 슬라이드 바에서 0 중복, 정상 슬라이드 이동되지 않는 현상

--- src/pages/clusters/containers/Resources/components/Modals/GpuClusters/Regist/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/GpuClusters/Regist/index.jsx
@@ -1178,7 +1178,7 @@ const RegistModal = props => {
   }
 
   const getMarks = max => {
-    const count = 5
+    const count = max < 4 ? max+1 : 5
     return range(count).reduce((marks, index) => {
       const value = (max * index) / (count - 1)
       const mark = value === 0 ? '0' : `${Math.floor(value)}`
