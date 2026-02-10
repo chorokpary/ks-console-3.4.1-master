b0131400066d2985a22886448377fcd8a088cf6e
Jian Li <pyguni@gmail.com>
2026-02-10 06:23:58 -0500

    Revert "Feat: enumerate the petasus resource on dedicated network creation"
    This reverts commit 2a70b310044176e41fcfefd0188cb2cd574df3c7.

--- src/pages/clusters/containers/Resources/components/Modals/PhysicalNetworks/Regist/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/PhysicalNetworks/Regist/index.jsx
@@ -37,11 +37,8 @@ const RegistModal = props => {
   useEffect(() => {
     const getNetResourceData = async () => {
       const listHostdevice = await hostdeviceStore.fetchList()
-      const filteredNames = listHostdevice.filter(device => device.is_net).map(device => device.name)
-      const listPetasusresource = await hostdeviceStore.fetchListPetasusResources()
-      const petasusResourceNames = listPetasusresource.resources
-      const combinedUniqueNames = [...new Set([...filteredNames, ...petasusResourceNames])];
-      setNetResource(combinedUniqueNames)
+      const filtered_names = listHostdevice.filter(device => device.is_net).map(device => device.name)
+      setNetResource(filtered_names)
     };
 
     getNetResourceData();
