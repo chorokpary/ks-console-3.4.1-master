48bfa4f35b006ca8648eb1fb7877d09c970faef6
Jian Li <pyguni@gmail.com>
2026-02-11 20:58:59 +0900

    Feat: add fabric and segmentId fields into network storage

--- src/pages/clusters/containers/Resources/components/Modals/NetworkStorages/Regist/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/NetworkStorages/Regist/index.jsx
@@ -51,15 +51,6 @@ const RegistModal = props => {
     { label: '6', value: '6' },
   ]
 
-  const interfaceOptions = [
-    { label: '0', value: '0' },
-    { label: '1', value: '1' },
-    { label: '2', value: '2' },
-    { label: '3', value: '3' },
-    { label: '4', value: '4' },
-    { label: '5', value: '5' },
-  ]
-
   const configProtocol = protocol => {
     if (protocol === "lustre" || protocol === "gpfs") {
       setDisableFilesystem(true)
@@ -242,23 +233,6 @@ const RegistModal = props => {
                       ]}
                     >
                       <Input name="segment_id" />
-                    </Form.Item>
-                  </Column>
-                  <Column>
-                    <Form.Item
-                      label={t('RESOURCES_INTERFACE')}
-                      rules={[
-                        {
-                          required: true,
-                          message: t('RESOURCES_INTERFACE_TIP'),
-                        },
-                      ]}
-                    >
-                      <Select
-                        name="interface"
-                        placeholder={t('RESOURCES_SELECT')}
-                        options={interfaceOptions}
-                      />
                     </Form.Item>
                   </Column>
                 </Columns>
