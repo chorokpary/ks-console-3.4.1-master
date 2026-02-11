a66bce48d5bb29c2e59002405d31a9aed4fea083
Jian Li <pyguni@gmail.com>
2026-02-11 20:58:59 +0900

    Feat: add fabric and segmentId fields into network storage

--- src/pages/clusters/containers/Resources/components/Modals/NetworkStorages/Modify/index.jsx
+++ src/pages/clusters/containers/Resources/components/Modals/NetworkStorages/Modify/index.jsx
@@ -3,7 +3,8 @@ import { Modal, TypeSelect, List, Panel } from 'components/Base';
 import { ProjectSelect } from 'components/Inputs';
 import {
   PATTERN_USER_NAME,
-  PATTERN_FILE_PATH
+  PATTERN_FILE_PATH,
+  PATTERN_SEGMENT_ID
 } from 'utils/constants';
 import { Form, Input, Select, Button, Tooltip, TextArea } from '@kube-design/components';
 import { Column, Columns } from '@kube-design/components/lib/components/Layout';
@@ -40,6 +41,11 @@ const ModifyModal = props => {
   const transportOptions = [
     { label: 'TCP', value: 'tcp' },
     { label: 'RDMA', value: 'rdma' },
+  ]
+
+  const fabricOptions = [
+    { label: 'INFINIBAND', value: 'infiniband' },
+    { label: 'ETHERNET', value: 'ethernet' },
   ]
 
   const maxConnectionOptions = [
@@ -177,6 +183,36 @@ const ModifyModal = props => {
                       />
                     </Form.Item>
                   </Column>
+                </Columns>
+              </Form.Item>
+            </Form.Group>
+          </Form.Item>
+
+          <Form.Item label={t('RESOURCES_NETWORK')}>
+            <Form.Group>
+              <Form.Item>
+                <Columns>
+                  <Column>
+                    <Form.Item
+                      label={t('RESOURCES_FABRIC')}
+                      rules={[
+                        {
+                          required: true,
+                          message: t('RESOURCES_SELECT_FABRIC_TIP'),
+                        },
+                      ]}
+                    >
+                      <Select
+                        name="fabric"
+                        placeholder={t('RESOURCES_SELECT')}
+                        options={fabricOptions}
+                        defaultValue={props.store.detail.network_storage.fabric}
+                        onChange={e => {
+                          configProtocol(e)
+                        }}
+                      />
+                    </Form.Item>
+                  </Column>
                   <Column>
                     <Form.Item
                       label={t('RESOURCES_TRANSPORT')}
@@ -190,15 +226,34 @@ const ModifyModal = props => {
                       <Select
                         name="transport"
                         placeholder={t('RESOURCES_SELECT')}
-                        options={transportOptions}
                         defaultValue={props.store.detail.network_storage.transport}
+                        options={transportOptions}
                       />
+                    </Form.Item>
+                  </Column>
+                  <Column>
+                    <Form.Item
+                      label={t('RESOURCES_SEGMENT_ID')}
+                      rules={[
+                        {
+                          required: true,
+
+                        },
+                        {
+                          pattern: PATTERN_SEGMENT_ID,
+                          message: t('RESOURCES_INVALID_SEGMENT_ID_DESC'),
+                        },
+                      ]}
+                    >
+                      <Input name="segment_id"
+                        defaultValue={props.store.detail.network_storage.segment_id} />
                     </Form.Item>
                   </Column>
                 </Columns>
               </Form.Item>
             </Form.Group>
           </Form.Item>
+
           <Form.Item label={t('RESOURCES_NETWORK_STORAGE_MOUNT')}>
             <Form.Group>
               <Form.Item>
