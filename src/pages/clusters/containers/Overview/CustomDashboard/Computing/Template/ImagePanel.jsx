a94921f12768d29942587a3f81d4e1b1327ac4ee
Jian Li <pyguni@gmail.com>
2026-02-20 21:56:33 +0900

    Feat: add link to computing template and network widgets' title

--- src/pages/clusters/containers/Overview/CustomDashboard/Computing/Template/ImagePanel.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/Computing/Template/ImagePanel.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Panel = ({ image }) => {
+const Panel = ({ cluster, image }) => {
 
     return (
         <>
             <div className="box type_status">
-                <h5><i className="ico-type24-image"></i>{t('RESOURCES_IMAGE')}</h5>
+                <h5><i className="ico-type24-image"></i><Link to={`/clusters/${cluster}/images`}>{t('RESOURCES_IMAGE')}</Link></h5>
                 <div className="cont_group">
                     <div className="cont1">
                         <div className="number_wrap">
--- src/pages/clusters/containers/Overview/CustomDashboard/Computing/Template/MediatedDevicePanel.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/Computing/Template/MediatedDevicePanel.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Panel = ({ md }) => {
+const Panel = ({ cluster, md }) => {
 
     return (
         <>
             <div className="box type_status">
-                <h5><i className="ico-type24-mediatedvgpu"></i>{t('RESOURCES_MEDIATED_DEVICE')}</h5>
+                <h5><i className="ico-type24-mediatedvgpu"></i><Link to={`/clusters/${cluster}/mediateddevices`}>{t('RESOURCES_MEDIATED_DEVICE')}</Link></h5>
                 <div className="cont_group">
                     <div className="cont1">
                         <div className="number_wrap">
