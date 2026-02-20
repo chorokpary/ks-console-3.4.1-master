ad17675e9c6fe6db09578d66f7a654d5ffbdc631
Jian Li <pyguni@gmail.com>
2026-02-20 21:56:33 +0900

    Feat: add link to computing template and network widgets' title

--- src/pages/clusters/containers/Overview/CustomDashboard/Computing/Network/NetworkPanel.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/Computing/Network/NetworkPanel.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Panel = ({ cluster, network }) => {
-    const history = useHistory();
 
     return (
         <>
-            <div className="box type_status" onClick={() => history.push(`/clusters/${cluster}/networks`)} style={{ cursor: 'pointer' }}>
-                <h5><i className="ico-type24-network"></i>{t('RESOURCES_NETWORK')}</h5>
+            <div className="box type_status">
+                <h5><i className="ico-type24-network"></i><Link to={`/clusters/${cluster}/networks`}>{t('RESOURCES_NETWORK')}</Link></h5>
                 <div className="cont_group">
                     <div className="cont1">
                         <div className="number_wrap">
