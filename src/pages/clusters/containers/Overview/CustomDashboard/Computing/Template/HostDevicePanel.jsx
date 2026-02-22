0a65773edf0b7bce999ea4c8949ad31bc42f922c
Jian Li <pyguni@gmail.com>
2026-02-22 17:44:38 +0900

    Fix: add link to div box instead of text for compute and net widget

--- src/pages/clusters/containers/Overview/CustomDashboard/Computing/Template/HostDevicePanel.jsx
+++ src/pages/clusters/containers/Overview/CustomDashboard/Computing/Template/HostDevicePanel.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 const Panel = ({ cluster, hd }) => {
+    const history = useHistory();
 
     return (
         <>
-            <div className="box type_status">
-                <h5><i className="ico-type24-hostdevice"></i><Link to={`/clusters/${cluster}/hostdevices`}>{t('RESOURCES_HOST_DEVICE')}</Link></h5>
+            <div className="box type_status" onClick={() => history.push(`/clusters/${cluster}/hostdevices`)} style={{ cursor: 'pointer' }}>
+                <h5><i className="ico-type24-hostdevice"></i>{t('RESOURCES_HOST_DEVICE')}</h5>
                 <div className="cont_group">
                     <div className="cont1">
                         <div className="number_wrap">
