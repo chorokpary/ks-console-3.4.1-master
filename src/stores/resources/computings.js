0e204d4b56a2ff7c55b0ac4f67f3d480b240ff6c
Jian Li <pyguni@gmail.com>
2026-02-24 15:32:20 +0900

    Feat: add KaaS and KaaS images into the project overview page

--- src/stores/resources/computings.js
+++ src/stores/resources/computings.js
@@ -35,24 +35,27 @@ export default class ComputingStore extends Base {
   } = {}) {
     this.isLoading = true
 
-    const apiUrl = `kapis/edgestack.kubesphere.io/v1alpha1${this.getPath({ cluster, namespace })}/edgetron/resources/kubevirt`;
+    const kubevirtApiUrl = `kapis/edgestack.kubesphere.io/v1alpha1${this.getPath({ cluster, namespace })}/edgetron/resources/kubevirt`;
+    const capkApiUrl = `kapis/edgestack.kubesphere.io/v1alpha1${this.getPath({ cluster, namespace })}/edgetron/resources/capk`
 
     const apiArray = [
-      { num: 1, type: 'vms', root: 'vms', name: t('RESOURCES_VM'), routeName: 'vms', multitenancy: true, icon: 'ico-type-vm', createField: 'creation_timestamp' },
-      { num: 2, type: 'images', root: 'images', name: t('RESOURCES_VM_IMAGE'), routeName: 'images', multitenancy: false, icon: 'snapshot', createField: 'timestamp' },
-      { num: 3, type: 'volumes', root: 'volumes', name: t('RESOURCES_VOLUME'), routeName: 'resourcesVolumes', multitenancy: true, icon: 'storage', createField: 'timestamp' },
-      { num: 4, type: 'network_storages', root: 'network_storages', name: t('RESOURCES_NETWORK_STORAGE'), routeName: 'networkstorages', multitenancy: true, icon: 'storage', createField: 'timestamp' },
-      { num: 5, type: 'flavors', root: 'flavors', name: t('RESOURCES_FLAVOR'), routeName: 'flavors', multitenancy: false, icon: 'apps', createField: 'timestamp' },
-      { num: 6, type: 'keypairs', root: 'keypairs', name: t('RESOURCES_KEYPAIR'), routeName: 'keypairs', multitenancy: true, icon: 'key', createField: 'timestamp' },
-      { num: 7, type: 'networks', root: 'networks', name: t('RESOURCES_NETWORK'), routeName: 'networks', multitenancy: true, icon: 'network-duotone', createField: 'timestamp' },
-      { num: 8, type: 'sriov_networks', root: 'sriovs', name: t('RESOURCES_SR_IOV_NETWORK'), routeName: 'sriovs', multitenancy: true, icon: 'ico-type-sriov', createField: 'timestamp' },
-      { num: 9, type: 'physical_networks', root: 'physicalnetworks', name: t('RESOURCES_DEDICATED_NETWORK'), routeName: 'physicalnetworks', multitenancy: true, icon: 'ico-type-sriov', createField: 'timestamp' },
-      { num: 10, type: 'routers', root: 'routers', name: t('RESOURCES_VROUTER'), routeName: 'routers', multitenancy: true, icon: 'router', createField: 'timestamp' },
-      { num: 11, type: 'floating_ips', root: 'floating_ips', name: t('RESOURCES_FLOATING_IP'), routeName: 'floatingip', multitenancy: true, icon: 'intranet-routers', createField: 'not' },
-      { num: 12, type: 'lbs', root: 'lbs', name: t('RESOURCES_LOAD_BALANCER'), routeName: 'loadBalancers', multitenancy: true, icon: 'loadbalancer', createField: 'timestamp' },
-      { num: 13, type: 'security_groups', root: 'security_groups', name: t('RESOURCES_SECURITY_GROUP'), routeName: 'securityGroups', multitenancy: true, icon: 'shield', createField: 'timestamp' },
-      { num: 14, type: 'host_devices', root: 'host_devices', name: t('RESOURCES_HOST_DEVICE'), routeName: 'hostdevices', multitenancy: false, icon: 'ico-type-hostdevice', createField: 'timestamp' },
-      { num: 15, type: 'mediated_devices', root: 'mediated_devices', name: t('RESOURCES_MEDIATED_DEVICE'), routeName: 'mediateddevices', multitenancy: false, icon: 'ico-type-mediatedvgpu', createField: 'timestamp' },
+      { num: 1, category: 'kubevirt', type: 'vms', root: 'vms', name: t('RESOURCES_VM'), routeName: 'vms', multitenancy: true, icon: 'ico-type-vm', createField: 'creation_timestamp' },
+      { num: 2, category: 'capk', type: 'clusters', root: 'clusters', name: t('RESOURCES_KAAS_RESOURCE'), routeName: 'containerResource', multitenancy: true, icon: 'kubernetes', createField: 'timestamp' },
+      { num: 3, category: 'kubevirt', type: 'images', root: 'images', name: t('RESOURCES_VM_IMAGE'), routeName: 'images', multitenancy: false, icon: 'snapshot', createField: 'timestamp' },
+      { num: 4, category: 'capk',type: 'images', root: 'images', name: t('RESOURCES_KAAS_IMAGE'), routeName: 'containerImages', multitenancy: false, icon: 'snapshot', createField: 'timestamp' },
+      { num: 5, category: 'kubevirt', type: 'volumes', root: 'volumes', name: t('RESOURCES_VOLUME'), routeName: 'resourcesVolumes', multitenancy: true, icon: 'storage', createField: 'timestamp' },
+      { num: 6, category: 'kubevirt', type: 'network_storages', root: 'network_storages', name: t('RESOURCES_NETWORK_STORAGE'), routeName: 'networkstorages', multitenancy: true, icon: 'storage', createField: 'timestamp' },
+      { num: 7, category: 'kubevirt', type: 'flavors', root: 'flavors', name: t('RESOURCES_FLAVOR'), routeName: 'flavors', multitenancy: false, icon: 'apps', createField: 'timestamp' },
+      { num: 8, category: 'kubevirt', type: 'keypairs', root: 'keypairs', name: t('RESOURCES_KEYPAIR'), routeName: 'keypairs', multitenancy: true, icon: 'key', createField: 'timestamp' },
+      { num: 9, category: 'kubevirt', type: 'networks', root: 'networks', name: t('RESOURCES_NETWORK'), routeName: 'networks', multitenancy: true, icon: 'network-duotone', createField: 'timestamp' },
+      { num: 10, category: 'kubevirt', type: 'sriov_networks', root: 'sriovs', name: t('RESOURCES_SR_IOV_NETWORK'), routeName: 'sriovs', multitenancy: true, icon: 'ico-type-sriov', createField: 'timestamp' },
+      { num: 11, category: 'kubevirt', type: 'physical_networks', root: 'physicalnetworks', name: t('RESOURCES_DEDICATED_NETWORK'), routeName: 'physicalnetworks', multitenancy: true, icon: 'ico-type-sriov', createField: 'timestamp' },
+      { num: 12, category: 'kubevirt', type: 'routers', root: 'routers', name: t('RESOURCES_VROUTER'), routeName: 'routers', multitenancy: true, icon: 'router', createField: 'timestamp' },
+      { num: 13, category: 'kubevirt', type: 'floating_ips', root: 'floating_ips', name: t('RESOURCES_FLOATING_IP'), routeName: 'floatingip', multitenancy: true, icon: 'intranet-routers', createField: 'not' },
+      { num: 14, category: 'kubevirt', type: 'lbs', root: 'lbs', name: t('RESOURCES_LOAD_BALANCER'), routeName: 'loadBalancers', multitenancy: true, icon: 'loadbalancer', createField: 'timestamp' },
+      { num: 15, category: 'kubevirt', type: 'security_groups', root: 'security_groups', name: t('RESOURCES_SECURITY_GROUP'), routeName: 'securityGroups', multitenancy: true, icon: 'shield', createField: 'timestamp' },
+      { num: 16, category: 'kubevirt', type: 'host_devices', root: 'host_devices', name: t('RESOURCES_HOST_DEVICE'), routeName: 'hostdevices', multitenancy: false, icon: 'ico-type-hostdevice', createField: 'timestamp' },
+      { num: 17, category: 'kubevirt', type: 'mediated_devices', root: 'mediated_devices', name: t('RESOURCES_MEDIATED_DEVICE'), routeName: 'mediateddevices', multitenancy: false, icon: 'ico-type-mediatedvgpu', createField: 'timestamp' },
     ];
 
     const computingDataArray = [];
@@ -64,10 +67,14 @@ export default class ComputingStore extends Base {
       let resultCount = 0;
 
       if(pagedModule.includes(item.type)){
-        result = get(await request.get(`${apiUrl}/${item.type}?project=${namespace}&limit=-1`), item.root, []);
+        result = get(await request.get(`${kubevirtApiUrl}/${item.type}?project=${namespace}&limit=-1`), item.root, []);
         resultCount = result.length;
       }else{
-        result = get(await request.get(`${apiUrl}/${item.type}`), item.root, []);
+        if (item.category == "kubevirt") {
+          result = get(await request.get(`${kubevirtApiUrl}/${item.type}`), item.root, []);
+        } else {
+          result = get(await request.get(`${capkApiUrl}/${item.type}`), item.root, []);
+        }
         resultCount = item.multitenancy ? result.filter(item => item.project == namespace).length : result.length;
       }
 
